955ad480001c19c94302953e1db385c475357900
Paul Copplestone <pcopplestone@gmail.com>
2019-11-20 12:09:43 +0000

    Testing: Cleaning up trigger from an old copy/paste

--- test/postgres/mnt/00-init.sql
+++ test/postgres/mnt/00-init.sql
@@ -22,45 +22,31 @@ VALUES
 CREATE FUNCTION public.broadcast_changes() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
-  DECLARE
-    current_row RECORD;
-	new_row_json jsonb;
-	old_row_json jsonb;
-	new_row_json_cleansed jsonb;
-	old_row_json_cleansed jsonb;
-  BEGIN
-    IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
-      current_row := NEW;
-    ELSE
-      current_row := OLD;
-    END IF;
-    IF (TG_OP = 'INSERT') THEN
-      OLD := NEW;
-    END IF;
-   
-   select into new_row_json to_json(NEW);
-   select into old_row_json to_json(OLD);
-  
-   -- It turns out that NOTIFY only supports up to 8000 bytes (~4000 chars?)
-   -- If it exceeds this limit it will throw an error.
-   -- To prevent this error & regression, we will never notify the changes to any column named "data".
-   -- Also we have to remove "schedule" because that is used in the assignments table, and it's how we found the bug/limitation
-   select into new_row_json_cleansed (new_row_json - 'data' - 'schedule')::jsonb;
-   select into old_row_json_cleansed (old_row_json - 'data' - 'schedule')::jsonb;
-  
-   PERFORM pg_notify(
-      'db_changes',
-      json_build_object(
-        'table', TG_TABLE_NAME,
-        'type', TG_OP,
-        'id', current_row.id,
-        'new_row_data', new_row_json_cleansed,
-        'old_row_data', old_row_json_cleansed
-      )::text
-    );
-  RETURN current_row;
-  END;
-  $$;
+      DECLARE
+        current_row RECORD;
+      BEGIN
+        IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
+          current_row := NEW;
+        ELSE
+          current_row := OLD;
+        END IF;
+        IF (TG_OP = 'INSERT') THEN
+          OLD := NEW;
+        END IF;
+      PERFORM pg_notify(
+          'db_changes',
+          json_build_object(
+            'table', TG_TABLE_NAME,
+            'type', TG_OP,
+            'id', current_row.id,
+            'new_row_data', row_to_json(NEW),
+            'old_row_data', row_to_json(OLD)
+          )::text
+        );
+      RETURN current_row;
+      END;
+      $$;
+
 
 
 -- Create the Replication publication 
