98982af1d5f92ff8c664c348fdee643bcf3dcade
Paul Copplestone <pcopplestone@gmail.com>
2019-12-11 16:47:00 +0800

    Removing boilerplate

--- server/lib/realtime_web/channels/realtime_channel.ex
+++ server/lib/realtime_web/channels/realtime_channel.ex
@@ -7,18 +7,12 @@ defmodule RealtimeWeb.RealtimeChannel do
     true
   end
 
-  def join("realtime", payload, socket) do    
+  def join("realtime", payload, socket) do
     if authorized?(payload) do
       { :ok, %{}, socket }
     else
       {:error, %{reason: "unauthorized"}}
     end
-  end
-
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
   end
 
   # It is also common to receive messages from the client and
@@ -32,7 +26,7 @@ defmodule RealtimeWeb.RealtimeChannel do
   # With this we can just send it a payload and it will "shout" it on the channel
   def handle_info(payload) do
     # Logger.info'REALTIME! #{inspect(payload)}'
-    
+
     # Optimally we would want subscriptions to be specific to the row || table name
     # RealtimeWeb.Endpoint.broadcast_from! self(), "realtime", payload.event, payload
 
