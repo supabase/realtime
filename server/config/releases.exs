a76408adc3346e533979a65bb402a4bb3982d6c5
Bobbie Soedirgo <bobbie@soedirgo.dev>
2020-06-05 20:25:02 +0800

    refactor: extract runtime config to releases.exs (#52)
    A lot of env-reading configs are sitting in application.ex. This should be refactored out into releases.exs, as per https://hexdocs.pm/phoenix/releases.html#runtime-configuration.

--- server/config/releases.exs
+++ server/config/releases.exs
@@ -1,28 +1,35 @@
 import Config
 
-app_port = System.get_env("PORT") || 4000
-app_hostname = System.get_env("HOSTNAME") || "localhost"
-db_user = System.get_env("DB_USER") || "postgres"
-db_password = System.get_env("DB_PASSWORD") || "postgres"
-db_host = System.get_env("DB_HOST") || "localhost"
-db_port = System.get_env("DB_PORT") || 5432
-db_name = System.get_env("DB_NAME") || "postgres"
-db_ssl = System.get_env("DB_SSL") || true
+app_hostname = System.get_env("HOSTNAME")
+app_port = System.get_env("PORT")
+db_host = System.get_env("DB_HOST")
+db_port = System.get_env("DB_PORT")
+db_name = System.get_env("DB_NAME")
+db_user = System.get_env("DB_USER")
+db_password = System.get_env("DB_PASSWORD")
+db_ssl = System.get_env("DB_SSL")
+slot_name = System.get_env("SLOT_NAME")
+configuration_file = System.get_env("CONFIGURATION_FILE")
+
+config :realtime,
+  app_hostname: app_hostname,
+  app_port: app_port,
+  db_host: db_host,
+  db_port: db_port,
+  db_name: db_name,
+  db_user: db_user,
+  db_password: db_password,
+  db_ssl: db_ssl,
+  slot_name: slot_name,
+  configuration_file: configuration_file
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
     raise """
     environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling:
-    export SECRET_KEY_BASE=`openssl rand -base64 48`
+    You can generate one by calling: mix phx.gen.secret
     """
 
 config :realtime, RealtimeWeb.Endpoint,
   http: [:inet6, port: String.to_integer(app_port)],
   secret_key_base: secret_key_base
-
-config :realtime,
-  app_port: app_port
-
-config :realtime,
-  app_hostname: app_hostname
