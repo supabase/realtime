d4c9eced516518f26d0174cc86508dc9530d0738
Paul Copplestone <pcopplestone@gmail.com>
2020-06-08 09:30:13 +0800

    Separates the channel subscription and the join functions. Demonstration for #43

--- examples/node-js/src/server.js
+++ examples/node-js/src/server.js
@@ -7,25 +7,22 @@ const socket = new Socket(REALTIME_URL)
 socket.connect()
 
 // Set up database listener
-const DatabaseListener = socket
-  .channel('realtime:*')
-  .join()
+const DatabaseListener = socket.channel('realtime:*')
+DatabaseListener.join()
   .receive('ok', () => console.log('DatabaseListener connected '))
   .receive('error', () => console.log('Failed'))
   .receive('timeout', () => console.log('Waiting...'))
 
 // Set up schema listener (public schema)
-const SchemaListener = socket
-  .channel('realtime:public')
-  .join()
+const SchemaListener = socket.channel('realtime:public')
+SchemaListener.join()
   .receive('ok', () => console.log('SchemaListener connected '))
   .receive('error', () => console.log('Failed'))
   .receive('timeout', () => console.log('Waiting...'))
 
 // Set up table listener (users table)
-const TableListener = socket
-  .channel('realtime:public:users')
-  .join()
+const TableListener = socket.channel('realtime:public:users')
+TableListener.join()
   .receive('ok', () => console.log('TableListener connected '))
   .receive('error', () => console.log('Failed'))
   .receive('timeout', () => console.log('Waiting...'))
@@ -33,19 +30,19 @@ const TableListener = socket
 /**
  * Log changes
  */
-DatabaseListener.channel.on('*', (change) => {
+DatabaseListener.on('*', (change) => {
   console.log('Change received on DatabaseListener', change)
 })
-SchemaListener.channel.on('*', (change) => {
+SchemaListener.on('*', (change) => {
   console.log('Change received on SchemaListener', change)
 })
-TableListener.channel.on('INSERT', (change) => {
+TableListener.on('INSERT', (change) => {
   console.log('INSERT on TableListener', change)
 })
-TableListener.channel.on('UPDATE', (change) => {
+TableListener.on('UPDATE', (change) => {
   console.log('UPDATE on TableListener', change)
 })
-TableListener.channel.on('DELETE', (change) => {
+TableListener.on('DELETE', (change) => {
   console.log('UPDATE on TableListener', change)
 })
 
