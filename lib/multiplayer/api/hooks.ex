5488191b1145139556993acc9ea455b8f2e0acd9
abc3 <abc3erl@protonmail.com>
2021-10-18 15:50:20 +0300

    set binary id in hooks table

--- lib/multiplayer/api/hooks.ex
+++ lib/multiplayer/api/hooks.ex
@@ -2,10 +2,14 @@ defmodule Multiplayer.Api.Hooks do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "hooks" do
     field :event, :string
     field :type, :string
     field :url, :string
+
+    belongs_to :project, Multiplayer.Api.Project
 
     timestamps()
   end
--- priv/repo/migrations/20211015181055_create_hooks.exs
+++ priv/repo/migrations/20211015181055_create_hooks.exs
@@ -2,7 +2,8 @@ defmodule Multiplayer.Repo.Migrations.CreateHooks do
   use Ecto.Migration
 
   def change do
-    create table(:hooks) do
+    create table(:hooks, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :type, :string
       add :event, :string
       add :url, :string
