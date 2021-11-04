da103acc59dfaf6f63dfc014df946e14412f15ae
abc3 <abc3erl@protonmail.com>
2021-11-04 16:18:24 +0200

    add tests for the rls case for realtime_channel

--- server/test/realtime_web/channels/realtime_channel_test.exs
+++ server/test/realtime_web/channels/realtime_channel_test.exs
@@ -2,14 +2,31 @@ defmodule RealtimeWeb.RealtimeChannelTest do
   use RealtimeWeb.ChannelCase
   require Logger
   import Realtime.Helpers, only: [broadcast_change: 2]
+  import Mock
+  alias RealtimeWeb.{ChannelsAuthorization, UserSocket, RealtimeChannel}
 
-  setup do
+  @token "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYzMjI4MzE5MSwiZXhwIjoxNjYzODE5MjExLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwic3ViIjoiYmJiNTFlNGUtZjM3MS00NDYzLWJmMGEtYWY4ZjU2ZGM5YTczIn0.imL7XhNMrS523vvdzQ93iRIw3OhjJutamLEoiZnJDbI"
+  @secret "d3v_HtNXEpT+zfsyy1LE1WPGmNKLWRfw/rpjnVtCEEM2cSFV2s+kUh5OKX7TPYmG"
+  @user_id "bbb51e4e-f371-4463-bf0a-af8f56dc9a73"
+
+  def setup_stream(_contex) do
     {:ok, _, socket} =
       socket(RealtimeWeb.UserSocket, "user_id", %{some: :assign})
       |> subscribe_and_join(RealtimeWeb.RealtimeChannel, "realtime:*")
 
     {:ok, socket: socket}
   end
+
+  def setup_rls(_contex) do
+    with_mock ChannelsAuthorization, authorize: fn _token -> {:ok, %{"sub" => @user_id}} end do
+      {:ok, _, socket} =
+        socket(UserSocket)
+        |> subscribe_and_join(RealtimeChannel, "realtime:*",  %{"user_token" => @token})
+      {:ok, socket: socket}
+    end
+  end
+
+  setup [:setup_stream, :setup_rls]
 
   test "INSERT message is pushed to the client" do
     change = %{
@@ -20,7 +37,7 @@ defmodule RealtimeWeb.RealtimeChannelTest do
 
     broadcast_change("realtime:*", change)
 
-    assert_push("INSERT", change)
+    assert_push("INSERT", ^change)
   end
 
   test "UPDATE message is pushed to the client" do
@@ -32,7 +49,7 @@ defmodule RealtimeWeb.RealtimeChannelTest do
 
     broadcast_change("realtime:*", change)
 
-    assert_push("UPDATE", change)
+    assert_push("UPDATE", ^change)
   end
 
   test "DELETE message is pushed to the client" do
@@ -44,6 +61,6 @@ defmodule RealtimeWeb.RealtimeChannelTest do
 
     broadcast_change("realtime:*", change)
 
-    assert_push("DELETE", change)
+    assert_push("DELETE", ^change)
   end
 end
