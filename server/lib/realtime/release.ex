c92560265fe573dc0f7d4779d8735ea89ce7be1b
Wen Bo Xie <wenbo.xie3@gmail.com>
2022-03-11 15:36:25 -0800

    feat: enable rollback in Realtime.Release

--- server/lib/realtime/release.ex
+++ server/lib/realtime/release.ex
@@ -1,13 +1,27 @@
 defmodule Realtime.Release do
+  alias Realtime.RLS
+
   @app :realtime
 
-  def migrate do
+  @spec migrate(String.t()) :: [Ecto.Repo.t()]
+  def migrate(prefix \\ "realtime") do
     load_app()
 
     for repo <- repos() do
       {:ok, _, _} =
-        Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true, prefix: "realtime"))
+        Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true, prefix: prefix))
     end
+  end
+
+  @spec rollback(integer(), String.t()) :: {:ok, [integer()], [Application.app()]}
+  def rollback(version, prefix \\ "realtime") do
+    load_app()
+
+    {:ok, _, _} =
+      Ecto.Migrator.with_repo(
+        RLS.Repo,
+        &Ecto.Migrator.run(&1, :down, to: version, prefix: prefix)
+      )
   end
 
   defp repos do
