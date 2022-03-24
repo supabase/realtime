c008a45630a5b2ed9d0ab2a2dc8dadd69ad98d2a
Wen Bo Xie <wenbo.xie3@gmail.com>
2022-03-24 14:30:01 +0100

    wip

---
+++ demo/client/RealtimeClient.ts
@@ -0,0 +1,67 @@
+import { RealtimeClient, RealtimeSubscription, RealtimePresence } from "@supabase/realtime-js"
+
+enum CHANNEL_EVENTS {
+  close = 'phx_close',
+  error = 'phx_error',
+  join = 'phx_join',
+  reply = 'phx_reply',
+  leave = 'phx_leave',
+  access_token = 'access_token',
+}
+
+export default class RealtimeClientV2 extends RealtimeClient {
+  constructor(endPoint: string, options?: { [key: string]: any }) {
+    super(endPoint, options)
+  }
+
+  channel(topic: string, chanParams = {}) {
+    const chan = new RealtimeSubscriptionV2(topic, chanParams, this)
+    this.channels.push(chan)
+    return chan
+  }
+}
+
+class RealtimeSubscriptionV2 extends RealtimeSubscription {
+  presence: RealtimePresence
+
+  constructor(topic: string, params: { [key: string]: any }, socket: RealtimeClientV2) {
+    super(topic, params, socket)
+
+    this.presence = new RealtimePresence(this)
+  }
+
+  list() {
+    return this.presence.list()
+  }
+
+  on(type: string, callback: Function, eventFilter?: { event: string, [key: string]: any }) {
+    this.bindings.push({ type, eventFilter: eventFilter ?? {}, callback })
+  }
+
+  trigger(type: string, payload?: any, ref?: string) {
+    const { close, error, leave, join } = CHANNEL_EVENTS
+    const events: string[] = [close, error, leave, join]
+    if (ref && events.indexOf(type) >= 0 && ref !== this.joinRef()) {
+      return
+    }
+    const handledPayload = this.onMessage(type, payload, ref)
+    if (payload && !handledPayload) {
+      throw 'channel onMessage callbacks must return the payload, modified or unmodified'
+    }
+
+    this.bindings
+      .filter((bind) => {
+        return bind?.type === type &&
+          (
+            bind?.eventFilter?.event === '*' ||
+            bind?.eventFilter?.event === payload?.event
+          )
+      })
+      .map((bind) => bind.callback(handledPayload, ref))
+  }
+
+  send(payload: { type: string, [key: string]: any }) {
+    const push = this.push(payload.type as any, payload)
+    console.log('push', push)
+  }
+}
--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -2,8 +2,39 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import RealtimeClientV2 from '../client/RealtimeClient'
+import { useEffect } from 'react'
+
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQ1MjEzMzE4LCJleHAiOjE5NjA3ODkzMTh9.KM08AjDI_zMMarPJfojM4A5Wg4Uv-ENg9AP3-B4E6Xk'
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    const client = new RealtimeClientV2('ws://dev_tenant.localhost:4000/socket', {
+      params: {
+        apikey: TOKEN
+      }
+    })
+
+    client.connect()
+
+    client.setAuth(TOKEN)
+
+    const channel = client.channel('realtime:*', {
+      user_token: TOKEN
+    })
+
+    channel.on('realtime', (payload: any) => console.log('payload', payload), { event: '*' })
+
+    channel.on('broadcast', (payload: any) => console.log('payload', payload), { event: 'POS' })
+
+    channel.subscribe()
+
+    channel.send({ type: 'broadcast', event: 'POS', payload: { x: 0, y: 0 } })
+    channel.send({ type: 'broadcast', event: 'POS', payload: { x: 0, y: 0 } })
+
+    console.log('channel', channel)
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
