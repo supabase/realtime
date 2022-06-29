a4ff73e2fa5643e55936a981e92ea7618fea3f9a
Wen Bo Xie <wenbo.xie3@gmail.com>
2022-06-29 11:19:40 -0700

    chore: update example to display changes from multi-tenant realtime

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,57 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Input, Button, Card, Table, Tag, Row, Col } from 'antd';
-import { RealtimeClient } from '@supabase/realtime-js'
+import { RealtimeClient } from '@supabase/realtime-js';
 
 export default function Index() {
   const [form] = Form.useForm();
   const [dataSource, setDataSource] = useState([]);
-  const [connButton, setConnButtonState] = useState({ loading: false, value: "Connect" });
-
+  const [connButton, setConnButtonState] = useState({
+    loading: false,
+    value: 'Connect',
+  });
 
   useEffect(() => {
     form.setFieldsValue({
       host: localStorage.getItem('host'),
       token: localStorage.getItem('token'),
-    })
-  }, [])
+    });
+  }, []);
 
   const onFinish = ({ host, token }) => {
-    setConnButtonState({ loading: true, value: "Connection ..." })
-    localStorage.setItem("host", host)
-    localStorage.setItem("token", token)
-    let socket = new RealtimeClient(host, { params: { apikey: token, vsndate: '2022-05-13' } })
-    socket.connect()
+    setConnButtonState({ loading: true, value: 'Connection ...' });
+    localStorage.setItem('host', host);
+    localStorage.setItem('token', token);
+    let socket = new RealtimeClient(host, {
+      params: { apikey: token, vsndate: '2022-05-13' },
+    });
+    socket.connect();
     const realtime_config = {
-      "configs": {
-        "broadcast": {
-          "filter": {
-            "event": "MESSAGE"
+      configs: {
+        broadcast: {
+          filter: {
+            event: 'MESSAGE',
           },
-          "type": "broadcast"
+          type: 'broadcast',
         },
-        "realtime": {
-          "filter": {
-            "event": "INSERT",
-            "schema": "public",
-            "table": "messages"
+        realtime: {
+          filter: {
+            event: 'INSERT',
+            schema: 'public',
+            table: 'messages',
           },
-          "type": "realtime"
-        }
+          type: 'realtime',
+        },
       },
-      "user_token": token
-    }
-    let channel = socket.channel('realtime:any')
+      user_token: token,
+    };
+    let channel = socket.channel('realtime:any');
 
     // userChannel.on('presence', { event: 'SYNC' }, () => {
     //   setIsInitialStateSynced(true)
     // })
     // userChannel.on('region', { event: '*' }, (region: string) => {
     //   setRegion(region)
-    // })    
+    // })
 
     // channel.on('*', msg => {
     channel.on('presence', { event: 'SYNC' }, (msg) => {
-      console.log("msg", msg)
+      console.log('msg', msg);
       // console.log('Got a message')
       // dataSource.unshift({
       //   key: dataSource.length + 1,
@@ -64,29 +68,42 @@ export default function Index() {
       //   ts: msg.commit_timestamp
       // })
       // setDataSource([...dataSource])
-    })
-    channel.on('realtime', {
-      event: '*',
-      schema: 'public',
-      table: '*',
-      // filter: 'field=eq.some_val'
-    }, (msg) => {
-      console.log("msg", msg)
-      // console.log('Got a message')
-      // dataSource.unshift({
-      //   key: dataSource.length + 1,
-      //   type: msg.type,
-      //   table: msg.schema + "." + msg.table,
-      //   record: JSON.stringify(msg.record),
-      //   old_record: JSON.stringify(msg.old_record),
-      //   errors: JSON.stringify(msg.errors),
-      //   columns: JSON.stringify(msg.columns),
-      //   ts: msg.commit_timestamp
-      // })
-      // setDataSource([...dataSource])
-    })
+    });
+    channel.on(
+      'realtime',
+      {
+        event: '*',
+        schema: 'public',
+        table: '*',
+        // filter: 'field=eq.some_val'
+      },
+      (msg) => {
+        console.log('database change', msg);
+        const {
+          columns,
+          commit_timestamp,
+          errors,
+          old_record,
+          record,
+          schema,
+          table,
+          type,
+        } = msg.payload;
+        dataSource.unshift({
+          key: dataSource.length + 1,
+          type: type,
+          table: schema + '.' + table,
+          record: JSON.stringify(record),
+          old_record: JSON.stringify(old_record),
+          errors: JSON.stringify(errors),
+          columns: JSON.stringify(columns),
+          ts: commit_timestamp,
+        });
+        setDataSource([...dataSource]);
+      }
+    );
     channel.on('broadcast', { event: 'MESSAGE' }, (msg) => {
-      console.log("broadcast", msg)
+      console.log('broadcast', msg);
       // console.log('Got a message')
       // dataSource.unshift({
       //   key: dataSource.length + 1,
@@ -99,17 +116,17 @@ export default function Index() {
       //   ts: msg.commit_timestamp
       // })
       // setDataSource([...dataSource])
-    })
+    });
     channel
       .subscribe()
       .receive('ok', () => {
-        let user_id = Math.random() + ""
+        let user_id = Math.random() + '';
         channel.send({
           type: 'presence',
           event: 'TRACK',
-          key: "newRoomId",
+          key: 'newRoomId',
           payload: { user_id: user_id },
-        })
+        });
 
         channel.send({
           type: 'broadcast',
@@ -118,13 +135,13 @@ export default function Index() {
             user_id: user_id,
             x: Math.random(),
           },
-        })
+        });
 
-        console.log('Connecting', channel)
-        setConnButtonState({ loading: false, value: "Connected" })
+        console.log('Connecting', channel);
+        setConnButtonState({ loading: false, value: 'Connected' });
       })
       .receive('error', () => console.log('Failed'))
-      .receive('timeout', () => console.log('Waiting...'))
+      .receive('timeout', () => console.log('Waiting...'));
   };
 
   const formItemLayout = {
@@ -132,44 +149,52 @@ export default function Index() {
     wrapperCol: { span: 14 },
   };
   const buttonItemLayout = {
-    wrapperCol: { span: 14, offset: 4 }
-  }
+    wrapperCol: { span: 14, offset: 4 },
+  };
 
-  const columns =
-    ["type", "table", "record", "old_record", "errors", "columns", "ts"]
-      .map(el => {
-        let column = {
-          title: el,
-          dataIndex: el,
-          key: el
+  const columns = [
+    'type',
+    'table',
+    'record',
+    'old_record',
+    'errors',
+    'columns',
+    'ts',
+  ].map((el) => {
+    let column = {
+      title: el,
+      dataIndex: el,
+      key: el,
+    };
+    if (el == 'type') {
+      column['render'] = (type) => {
+        let color = '#ccc';
+        switch (type) {
+          case 'INSERT':
+            color = 'green';
+            break;
+          case 'UPDATE':
+            color = 'blue';
+            break;
+          case 'DELETE':
+            color = 'red';
+            break;
         }
-        if (el == "type") {
-          column['render'] = type => {
-            let color = "#ccc"
-            switch (type) {
-              case "INSERT":
-                color = "green"
-                break;
-              case "UPDATE":
-                color = "blue"
-                break;
-              case "DELETE":
-                color = "red"
-                break;
-            }
-            return (
-              <Tag color={color} key={type}>{type}</Tag>
-            )
-          }
-          column['filters'] = [
-            { text: 'INSERT', value: 'INSERT' },
-            { text: 'UPDATE', value: 'UPDATE' },
-            { text: 'DELETE', value: 'DELETE' },
-          ]
-          column['onFilter'] = (value, record) => record.type == value
-        }
-        return column
-      })
+        return (
+          <Tag color={color} key={type}>
+            {type}
+          </Tag>
+        );
+      };
+      column['filters'] = [
+        { text: 'INSERT', value: 'INSERT' },
+        { text: 'UPDATE', value: 'UPDATE' },
+        { text: 'DELETE', value: 'DELETE' },
+      ];
+      column['onFilter'] = (value, record) => record.type == value;
+    }
+    return column;
+  });
 
   return (
     <>
@@ -187,7 +212,11 @@ export default function Index() {
             <Input.TextArea size="large" rows={4} />
           </Form.Item>
           <Form.Item {...buttonItemLayout}>
-            <Button type="primary" htmlType="submit" loading={connButton.loading}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              loading={connButton.loading}
+            >
               {connButton.value}
             </Button>
           </Form.Item>
