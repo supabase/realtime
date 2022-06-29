f0cce58721f15364b572b9320977b3357d68b9dc
Wen Bo Xie <wenbo.xie3@gmail.com>
2022-06-29 11:19:40 -0700

    chore: update example to display changes from multi-tenant realtime

--- example/pages/_app.js
+++ example/pages/_app.js
@@ -1,7 +1,7 @@
 import 'antd/dist/antd.dark.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
