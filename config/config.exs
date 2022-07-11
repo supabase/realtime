85ae2d6acb1d87827d97ef5737251999fa27fbd7
TzeYiing <ty@tzeyiing.com>
2022-07-11 15:37:01 +1000

    fix: update config metadata to include :project and :external_id 😄

--- config/config.exs
+++ config/config.exs
@@ -38,7 +38,7 @@ config :phoenix_swagger, json_library: Jason
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: [:request_id, :project, :external_id]
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
