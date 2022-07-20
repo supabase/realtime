ae53b2f7ee0c9d35b882cfb232864cbd086d0c31
abc3 <sts@abc3.dev>
2022-07-20 15:25:31 +0200

    do the jwt encryption migration in transaction

--- priv/repo/migrations/20220715115810_encrypt_jwt_secret.exs
+++ priv/repo/migrations/20220715115810_encrypt_jwt_secret.exs
@@ -8,15 +8,24 @@ defmodule Realtime.Repo.Migrations.EncryptJwtSecretEncrypted do
   def change do
     secure_key = System.get_env("DB_ENC_KEY")
 
-    from(t in Realtime.Api.Tenant, select: t)
-    |> Realtime.Repo.all()
-    |> Enum.each(fn e ->
-      %{
-        Realtime.Api.Tenant.changeset(e, %{})
-        | action: :update,
-          changes: %{jwt_secret: encrypt!(e.jwt_secret, secure_key)}
-      }
-      |> Realtime.Repo.update()
+    Repo.transaction(fn ->
+      from(t in Realtime.Api.Tenant, select: t)
+      |> Realtime.Repo.all()
+      |> Enum.each(fn e ->
+        %{
+          Realtime.Api.Tenant.changeset(e, %{})
+          | action: :update,
+            changes: %{jwt_secret: encrypt!(e.jwt_secret, secure_key)}
+        }
+        |> Realtime.Repo.update()
+      end)
+      |> case do
+        {:error, reason} ->
+          raise(reason)
+
+        _ ->
+          :ok
+      end
     end)
   end
 end
