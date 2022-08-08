b60e0abe8ca96128643777f457d3fe09cfda646c
Chase Granberry <chase@logflare.app>
2022-08-08 09:42:36 -0700

    No more `which_children` uses one counter per term

--- lib/realtime/gen_counter/gen_counter.ex
+++ lib/realtime/gen_counter/gen_counter.ex
@@ -1,6 +1,6 @@
 defmodule Realtime.GenCounter do
   @moduledoc """
-  A generic counter interface for any Erlang term.
+  A generic counter interface for any Erlang term. One counter per term.
   """
 
   use GenServer
@@ -64,6 +64,49 @@ defmodule Realtime.GenCounter do
     end
   end
 
+  @spec sub(term()) :: :ok | :error
+  def sub(term) do
+    sub(term, 1)
+  end
+
+  @spec sub(term(), integer()) :: :ok | :error
+  def sub(term, count) when is_integer(count) do
+    with {:ok, pid} <- find_worker(term),
+         {:ok, index} <- find_index(pid),
+         {:ok, counter_ref} <- find_counter(term) do
+      :counters.sub(counter_ref, index, count)
+    else
+      err ->
+        Logger.error("Error decrimenting counter", error_string: inspect(err))
+        :error
+    end
+  end
+
+  @spec put(term(), integer()) :: :ok | :error
+  def put(term, count) when is_integer(count) do
+    with {:ok, pid} <- find_worker(term),
+         {:ok, index} <- find_index(pid),
+         {:ok, counter_ref} <- find_counter(term) do
+      :counters.put(counter_ref, index, count)
+    else
+      err ->
+        Logger.error("Error updating counter", error_string: inspect(err))
+        :error
+    end
+  end
+
+  @spec info(term()) :: :ok | :error
+  def info(term) do
+    with {:ok, counter_ref} <-
+           find_counter(term) do
+      :counters.info(counter_ref)
+    else
+      err ->
+        Logger.error("Error", error_string: inspect(err))
+        :error
+    end
+  end
+
   @spec get(term()) ::
           {:ok, integer()} | {:error, :child_not_found | :worker_not_found | :counter_not_found}
   def get(term) do
@@ -95,8 +138,7 @@ defmodule Realtime.GenCounter do
 
   @impl true
   def handle_call(:new, _from, state) do
-    {:ok, index} = find_index(self())
-    ref = :counters.new(index, [:write_concurrency])
+    ref = :counters.new(1, [:write_concurrency])
 
     {:ok, _} = Registry.register(Realtime.Registry.Unique, {__MODULE__, :counter, state.id}, ref)
 
@@ -105,16 +147,8 @@ defmodule Realtime.GenCounter do
     {:reply, {:ok, ref}, %{state | counters: counters}}
   end
 
-  defp find_index(pid) do
-    children =
-      DynamicSupervisor.which_children(GenCounter.DynamicSupervisor)
-      |> Enum.with_index(1)
-      |> Enum.find(fn {{:undefined, worker_pid, :worker, _}, _index} -> pid == worker_pid end)
-
-    case children do
-      {_worker, index} -> {:ok, index}
-      _error -> {:error, :child_not_found}
-    end
+  defp find_index(_pid) do
+    {:ok, 1}
   end
 
   defp find_worker(term) do
