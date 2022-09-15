6e56e726d2ba789369dcc6d0bec8cfbba10dfb94
abc3 <sts@abc3.dev>
2022-09-15 20:56:40 +0200

    postgres connection, broadcast and presence tests

--- test/integration/rt_channel.exs
+++ test/integration/rt_channel.exs
@@ -3,16 +3,19 @@ Code.require_file("../support/websocket_client.exs", __DIR__)
 defmodule Phoenix.Integration.RtChannelTest do
   use RealtimeWeb.ConnCase
   import ExUnit.CaptureLog
+  alias Postgrex, as: P
 
   alias Phoenix.Integration.WebsocketClient
-  alias Phoenix.Socket.{V1, V2, Message}
+  alias Phoenix.Socket.{V1, Message}
   alias __MODULE__.Endpoint
+  alias Extensions.Postgres
 
   @moduletag :capture_log
   @port 5807
   @serializer V1.JSONSerializer
   @external_id "dev_tenant"
-  @token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQ5OTYzNTc1LCJleHAiOjE5NjU1Mzk1NzV9.v7UZK05KaVQKInBBH_AP5h0jXUEwCCC5qtdj3iaxbNQ"
+  @token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvY2FsaG9zdCIsInJvbGUiOiJhdXRoZW50aWNhdGVkIiwiaWF0IjoxNjU4NjAwNzkxLCJleHAiOjE5NzQxNzY3OTF9.asQn-i7DDicPbMN_cjr7QB01kUs1RxFy0CboLmMwZxg"
+  @uri "ws://#{@external_id}.localhost:#{@port}/socket/websocket?apikey=#{@token}&vsndate=2022&vsn=1.0.0"
 
   Application.put_env(:phoenix, Endpoint,
     https: false,
@@ -32,18 +35,19 @@ defmodule Phoenix.Integration.RtChannelTest do
                     key: "_hello_key",
                     signing_salt: "change_me"
 
-    socket "/socket", RealtimeWeb.UserSocket,
+    socket("/socket", RealtimeWeb.UserSocket,
       websocket: [
         connect_info: [:peer_data, :uri, :x_headers],
         fullsweep_after: 20,
         max_frame_size: 8_000_000
       ],
       longpoll: false
+    )
 
-    plug Plug.Session, @session_config
-    plug :fetch_session
-    plug Plug.CSRFProtection
-    plug :put_session
+    plug(Plug.Session, @session_config)
+    plug(:fetch_session)
+    plug(Plug.CSRFProtection)
+    plug(:put_session)
 
     defp put_session(conn, _) do
       conn
@@ -59,75 +63,145 @@ defmodule Phoenix.Integration.RtChannelTest do
     :ok
   end
 
-  @endpoint Endpoint
+  test "connection" do
+    socket = get_connection()
 
-  describe "connection" do
-    test "messages can be pushed and received" do
-      {:ok, socket} =
-        WebsocketClient.connect(
-          self(),
-          "ws://#{@external_id}.localhost:#{@port}/socket/websocket?apikey=#{@token}&vsndate=2022&vsn=1.0.0",
-          @serializer
-        )
+    config = %{
+      postgres_changes: [%{event: "*", schema: "public"}]
+    }
 
-      config = %{
-        broadcast: %{self: true},
-        presence: %{key: ""},
-        postgres_changes: [%{event: "*", schema: "public"}]
-      }
+    WebsocketClient.join(socket, "realtime:any", %{config: config})
+    id = :erlang.phash2(%{"event" => "*", "schema" => "public"})
 
-      WebsocketClient.join(socket, "realtime:any", %{config: config})
-
-      assert_receive %Message{
-        event: "phx_reply",
-        payload: %{
-          "response" => %{
-            "postgres_changes" => [%{"event" => "*", "id" => 74_307_548, "schema" => "public"}]
-          },
-          "status" => "ok"
+    assert_receive %Message{
+      event: "phx_reply",
+      payload: %{
+        "response" => %{
+          "postgres_changes" => [%{"event" => "*", "id" => ^id, "schema" => "public"}]
         },
-        join_ref: nil,
-        ref: "1",
-        topic: "realtime:any"
-      }
+        "status" => "ok"
+      },
+      join_ref: nil,
+      ref: "1",
+      topic: "realtime:any"
+    }
 
-      assert_receive %Message{
-        event: "presence_state",
-        join_ref: nil,
-        payload: %{},
-        ref: nil,
-        topic: "realtime:any"
-      }
+    assert_receive %Message{}
 
-      payload = %{
-        type: "presence",
-        event: "TRACK",
-        payload: %{name: "realtime_presence_96", t: 1814.7000000029802}
-      }
+    :timer.sleep(6000)
 
-      WebsocketClient.send_event(socket, "realtime:any", "presence", payload)
+    assert_receive %Message{
+      event: "system",
+      join_ref: nil,
+      payload: %{
+        "message" => "subscribed to realtime",
+        "status" => "ok",
+        "topic" => "dev_tenant:any"
+      },
+      ref: nil,
+      topic: "realtime:any"
+    }
+  end
 
-      assert_receive %Message{
-        event: "presence_diff",
-        join_ref: nil,
-        payload:
-          %{
-            # "joins" => %{
-            #   "e0db62a8-34fb-11ed-95f2-fe267df90fe2" => %{
-            #     "metas" => [
-            #       %{
-            #         "name" => "realtime_presence_96",
-            #         "phx_ref" => "FxUMVDdHmAbLngMi",
-            #         "t" => 1814.7000000029802
-            #       }
-            #     ]
-            #   }
-            # },
-            # "leaves" => %{}
-          },
-        ref: nil,
-        topic: "realtime:any"
-      }
-    end
+  test "broadcast" do
+    socket = get_connection()
+
+    config = %{
+      broadcast: %{self: true}
+    }
+
+    WebsocketClient.join(socket, "realtime:any", %{config: config})
+
+    assert_receive %Message{
+      event: "phx_reply",
+      payload: %{
+        "response" => %{
+          "postgres_changes" => []
+        },
+        "status" => "ok"
+      },
+      join_ref: nil,
+      ref: "1",
+      topic: "realtime:any"
+    }
+
+    assert_receive %Message{}
+
+    payload = %{"event" => "TEST", "payload" => %{"msg" => 1}, "type" => "broadcast"}
+    WebsocketClient.send_event(socket, "realtime:any", "broadcast", payload)
+
+    assert_receive %Message{
+      event: "broadcast",
+      join_ref: nil,
+      payload: ^payload,
+      ref: nil,
+      topic: "realtime:any"
+    }
+  end
+
+  test "presence" do
+    socket = get_connection()
+
+    config = %{
+      presence: %{key: ""}
+    }
+
+    WebsocketClient.join(socket, "realtime:any", %{config: config})
+
+    assert_receive %Message{
+      event: "phx_reply",
+      payload: %{
+        "response" => %{
+          "postgres_changes" => []
+        },
+        "status" => "ok"
+      },
+      join_ref: nil,
+      ref: "1",
+      topic: "realtime:any"
+    }
+
+    assert_receive %Message{
+      event: "presence_state",
+      join_ref: nil,
+      payload: %{},
+      ref: nil,
+      topic: "realtime:any"
+    }
+
+    payload = %{
+      type: "presence",
+      event: "TRACK",
+      payload: %{name: "realtime_presence_96", t: 1814.7000000029802}
+    }
+
+    WebsocketClient.send_event(socket, "realtime:any", "presence", payload)
+
+    assert_receive %Message{
+      event: "presence_diff",
+      join_ref: nil,
+      payload:
+        %{
+          # "joins" => %{
+          #   "e0db62a8-34fb-11ed-95f2-fe267df90fe2" => %{
+          #     "metas" => [
+          #       %{
+          #         "name" => "realtime_presence_96",
+          #         "phx_ref" => "FxUMVDdHmAbLngMi",
+          #         "t" => 1814.7000000029802
+          #       }
+          #     ]
+          #   }
+          # },
+          # "leaves" => %{}
+        },
+      ref: nil,
+      topic: "realtime:any"
+    }
+  end
+
+  defp get_connection() do
+    {:ok, socket} = WebsocketClient.connect(self(), @uri, @serializer)
+    socket
   end
 end
