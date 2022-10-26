529c85bf4b8b6a9b540eab6bfd9331367a96a171
abc3 <sts@abc3.dev>
2022-10-26 20:48:15 +0200

    Update lib/extensions/postgres_cdc_stream/supervisor.ex
    Co-authored-by: Wen Bo Xie <5532241+w3b6x9@users.noreply.github.com>

--- lib/extensions/postgres_cdc_stream/supervisor.ex
+++ lib/extensions/postgres_cdc_stream/supervisor.ex
@@ -15,7 +15,7 @@ defmodule Extensions.PostgresCdcStream.Supervisor do
     children = [
       {
         PartitionSupervisor,
-        partitions: 2,
+        partitions: 20,
         child_spec: DynamicSupervisor,
         strategy: :one_for_one,
         name: Stream.DynamicSupervisor
