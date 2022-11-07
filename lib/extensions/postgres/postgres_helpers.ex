c29f20fd814df3f4da3e6ba428aeb262487297ac
Wen Bo Xie <wenbo.xie3@gmail.com>
2022-11-07 15:49:54 -0800

    fix: change type to postgres_cdc_rls

--- lib/extensions/postgres/postgres_helpers.ex
+++ lib/extensions/postgres/postgres_helpers.ex
@@ -2,7 +2,7 @@ defmodule Extensions.Postgres.Helpers do
   def filter_postgres_settings(extensions) do
     [postgres] =
       Enum.filter(extensions, fn e ->
-        if e.type == "postgres" do
+        if e.type == "postgres_cdc_rls" do
           true
         else
           false
