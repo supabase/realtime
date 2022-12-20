e06e9404074d0abd27a8a1367e9418709265bf78
Wen Bo Xie <wenbo.xie3@gmail.com>
2022-12-19 23:03:18 -0800

    chore: run seeds.exs file in Docker command to seed local dev database

--- lib/realtime/release.ex
+++ lib/realtime/release.ex
@@ -18,6 +18,21 @@ defmodule Realtime.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  def seeds(repo) do
+    load_app()
+
+    {:ok, {:ok, _}, _} =
+      Ecto.Migrator.with_repo(repo, fn _repo ->
+        seeds_file = "#{:code.priv_dir(@app)}/repo/seeds.exs"
+
+        if File.regular?(seeds_file) do
+          {:ok, Code.eval_file(seeds_file)}
+        else
+          {:error, "Seeds file not found."}
+        end
+      end)
+  end
+
   defp repos do
     Application.fetch_env!(@app, :ecto_repos)
   end
