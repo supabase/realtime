bc4132077bd91ff881418c6c6737c7e90a763cb5
Wen Bo Xie <5532241+w3b6x9@users.noreply.github.com>
2022-12-20 16:39:20 -0800

    feat: streamline local Docker setup (#377)
    * feat: streamline local Docker setup
    
    * chore: run seeds.exs file in Docker command to seed local dev database

---
+++ priv/repo/seeds.exs
@@ -0,0 +1,34 @@
+alias Realtime.{Api.Tenant, Repo}
+
+tenant_name = "realtime-dev"
+
+Repo.transaction(fn ->
+  case Repo.get_by(Tenant, external_id: tenant_name) do
+    %Tenant{} = tenant -> Repo.delete!(tenant)
+    nil -> {:ok, nil}
+  end
+
+  %Tenant{}
+  |> Tenant.changeset(%{
+    "name" => tenant_name,
+    "external_id" => tenant_name,
+    "jwt_secret" => "a1d99c8b-91b6-47b2-8f3c-aa7d9a9ad20f",
+    "extensions" => [
+      %{
+        "type" => "postgres_cdc_rls",
+        "settings" => %{
+          "db_name" => System.get_env("DB_NAME") || "postgres",
+          "db_host" => System.get_env("DB_HOST") || "host.docker.internal",
+          "db_user" => System.get_env("DB_USER") || "postgres",
+          "db_password" => System.get_env("DB_PASSWORD") || "postgres",
+          "db_port" => System.get_env("DB_PORT") || 5432,
+          "region" => "us-east-1",
+          "poll_interval_ms" => 100,
+          "poll_max_record_bytes" => 1_048_576,
+          "ip_version" => 4
+        }
+      }
+    ]
+  })
+  |> Repo.insert!()
+end)
