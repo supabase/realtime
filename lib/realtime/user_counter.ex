df13640d5d7bff7211513f169240d2ed49dbbafa
Chase Granberry <chase@logflare.app>
2023-01-05 13:47:35 -0700

    fix: add specs to UsersCounter

--- lib/realtime/user_counter.ex
+++ lib/realtime/user_counter.ex
@@ -2,14 +2,17 @@ defmodule Realtime.UsersCounter do
   @moduledoc false
   require Logger
 
+  @spec add(pid(), String.t()) :: :ok
   def add(pid, tenant) do
     :syn.join(:users, tenant, pid)
   end
 
+  @spec tenant_users(String.t()) :: non_neg_integer()
   def tenant_users(tenant) do
     :syn.member_count(:users, tenant)
   end
 
+  @spec tenant_users(atom, String.t()) :: non_neg_integer()
   def tenant_users(node_name, tenant) do
     :syn.member_count(:users, tenant, node_name)
   end
