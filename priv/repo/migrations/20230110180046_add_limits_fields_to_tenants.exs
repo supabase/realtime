96a40ebf3e3648c22d85a6c2caab637db2d190d4
Chase Granberry <chase@logflare.app>
2023-01-10 11:16:14 -0700

    Add max_joins_per_second to schema

--- priv/repo/migrations/20230110180046_add_limits_fields_to_tenants.exs
+++ priv/repo/migrations/20230110180046_add_limits_fields_to_tenants.exs
@@ -5,6 +5,7 @@ defmodule Realtime.Repo.Migrations.AddLimitsFieldsToTenants do
     alter table("tenants") do
       add(:max_bytes_per_second, :integer, default: 100_000, null: false)
       add(:max_channels_per_client, :integer, default: 100, null: false)
+      add(:max_joins_per_second, :integer, default: 500, null: false)
     end
   end
 end
