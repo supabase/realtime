b19dbd1b5d890274b8652244309d997cc2ce60d3
Chase Granberry <chase@logflare.app>
2023-01-10 11:12:59 -0700

    Add max_bytes_per_second and max_channels_per_client fields

---
+++ priv/repo/migrations/20230110180046_add_limits_fields_to_tenants.exs
@@ -0,0 +1,10 @@
+defmodule Realtime.Repo.Migrations.AddLimitsFieldsToTenants do
+  use Ecto.Migration
+
+  def change do
+    alter table("tenants") do
+      add(:max_bytes_per_second, :integer, default: 100_000, null: false)
+      add(:max_channels_per_client, :integer, default: 100, null: false)
+    end
+  end
+end
