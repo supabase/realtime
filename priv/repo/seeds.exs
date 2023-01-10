fd7fd5c2d7596f0fa37a4993f62e63fc943438a3
Wen Bo Xie <wenbo.xie3@gmail.com>
2023-01-09 16:49:38 -0800

    feat: default realtime-dev JWT secret to API_JWT_SECRET env var value

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,16 +12,16 @@ Repo.transaction(fn ->
   |> Tenant.changeset(%{
     "name" => tenant_name,
     "external_id" => tenant_name,
-    "jwt_secret" => "a1d99c8b-91b6-47b2-8f3c-aa7d9a9ad20f",
+    "jwt_secret" => System.get_env("API_JWT_SECRET", "a1d99c8b-91b6-47b2-8f3c-aa7d9a9ad20f"),
     "extensions" => [
       %{
         "type" => "postgres_cdc_rls",
         "settings" => %{
-          "db_name" => System.get_env("DB_NAME") || "postgres",
-          "db_host" => System.get_env("DB_HOST") || "host.docker.internal",
-          "db_user" => System.get_env("DB_USER") || "postgres",
-          "db_password" => System.get_env("DB_PASSWORD") || "postgres",
-          "db_port" => System.get_env("DB_PORT") || 5432,
+          "db_name" => System.get_env("DB_NAME", "postgres"),
+          "db_host" => System.get_env("DB_HOST", "host.docker.internal"),
+          "db_user" => System.get_env("DB_USER", "postgres"),
+          "db_password" => System.get_env("DB_PASSWORD", "postgres"),
+          "db_port" => System.get_env("DB_PORT", "5432"),
           "region" => "us-east-1",
           "poll_interval_ms" => 100,
           "poll_max_record_bytes" => 1_048_576,
