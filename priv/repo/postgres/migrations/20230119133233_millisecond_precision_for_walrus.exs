d9011aad36d3d6fac41c0beb525c368d890cff05
Wen Bo Xie <wenbo.xie3@gmail.com>
2023-01-29 13:11:10 -0800

    fix: update walrus db change ms timestamp migration

--- priv/repo/postgres/migrations/20230119133233_millisecond_precision_for_walrus.exs
+++ priv/repo/postgres/migrations/20230119133233_millisecond_precision_for_walrus.exs
@@ -1,4 +1,4 @@
-defmodule Realtime.Repo.Migrations.MilliseconPrecisionForWalrus do
+defmodule Realtime.RLS.Repo.Migrations.MillisecondPrecisionForWalrus do
   use Ecto.Migration
 
   def change do
@@ -189,43 +189,43 @@ defmodule Realtime.Repo.Migrations.MilliseconPrecisionForWalrus do
                   -- Add \"record\" key for insert and update
                   || case
                       when action in ('INSERT', 'UPDATE') then
-                          case
-                              when error_record_exceeds_max_size then
-                                  jsonb_build_object(
-                                      'record',
-                                      (
-                                          select jsonb_object_agg((c).name, (c).value)
-                                          from unnest(columns) c
-                                          where (c).is_selectable and (octet_length((c).value::text) <= 64)
-                                      )
-                                  )
-                              else
-                                  jsonb_build_object(
-                                      'record',
-                                      (select jsonb_object_agg((c).name, (c).value) from unnest(columns) c where (c).is_selectable)
-                                  )
-                          end
+                          jsonb_build_object(
+                              'record',
+                              (
+                                  select jsonb_object_agg((c).name, (c).value)
+                                  from unnest(columns) c
+                                  where
+                                      (c).is_selectable
+                                      and ( not error_record_exceeds_max_size or (octet_length((c).value::text) <= 64))
+                              )
+                          )
                       else '{}'::jsonb
                   end
                   -- Add \"old_record\" key for update and delete
                   || case
-                      when action in ('UPDATE', 'DELETE') then
-                          case
-                              when error_record_exceeds_max_size then
-                                  jsonb_build_object(
-                                      'old_record',
-                                      (
-                                          select jsonb_object_agg((c).name, (c).value)
-                                          from unnest(old_columns) c
-                                          where (c).is_selectable and (octet_length((c).value::text) <= 64)
-                                      )
+                      when action = 'UPDATE' then
+                          jsonb_build_object(
+                                  'old_record',
+                                  (
+                                      select jsonb_object_agg((c).name, (c).value)
+                                      from unnest(old_columns) c
+                                      where
+                                          (c).is_selectable
+                                          and ( not error_record_exceeds_max_size or (octet_length((c).value::text) <= 64))
                                   )
-                              else
-                                  jsonb_build_object(
-                                      'old_record',
-                                      (select jsonb_object_agg((c).name, (c).value) from unnest(old_columns) c where (c).is_selectable)
-                                  )
-                          end
+                              )
+                      when action = 'DELETE' then
+                          jsonb_build_object(
+                              'old_record',
+                              (
+                                  select jsonb_object_agg((c).name, (c).value)
+                                  from unnest(old_columns) c
+                                  where
+                                      (c).is_selectable
+                                      and ( not error_record_exceeds_max_size or (octet_length((c).value::text) <= 64))
+                                      and ( not is_rls_enabled or (c).is_pkey ) -- if RLS enabled, we can't secure deletes so filter to pkey
+                              )
+                          )
                       else '{}'::jsonb
                   end;
 
