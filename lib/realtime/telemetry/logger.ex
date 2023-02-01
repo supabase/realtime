dbf5a224143c556ba99ef5dda76b1bd42c17d69c
Chase Granberry <chase@logflare.app>
2023-02-01 10:18:13 -0700

    Working

---
+++ lib/realtime/telemetry/logger.ex
@@ -0,0 +1,35 @@
+defmodule Realtime.Telemetry.Logger do
+  @moduledoc """
+  We can log less frequent Telemetry events to get data into BigQuery.
+  """
+
+  require Logger
+
+  use GenServer
+
+  @events [[:realtime, :connections]]
+
+  def start_link(args \\ []) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(_args) do
+    :telemetry.attach_many(
+      <<"telemetry-logger">>,
+      @events,
+      &__MODULE__.handle_event/4,
+      []
+    )
+
+    {:ok, []}
+  end
+
+  def handle_event(event, measurements, %{tenant: tenant}, _config) do
+    meta = %{project: tenant, measurements: measurements}
+    Logger.info(["Billing metrics: ", inspect(event)], meta)
+  end
+
+  def handle_event(_event, _measurements, _metadata, _config) do
+    :ok
+  end
+end
