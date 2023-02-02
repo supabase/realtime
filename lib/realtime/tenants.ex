d6e0136f5a6b758d72b2715f593e9af317179cbe
Chase Granberry <chase@logflare.app>
2023-02-02 12:53:06 -0700

    List tenants for a node only

--- lib/realtime/tenants.ex
+++ lib/realtime/tenants.ex
@@ -1,2 +1,19 @@
 defmodule Realtime.Tenants do
+  @moduledoc """
+  Everything to do with Tenants.
+  """
+
+  @doc """
+  Gets a list of connected tenant `external_id` strings in the cluster or a node.
+  """
+
+  @spec list_connected_tenants :: [String.t()]
+  def list_connected_tenants() do
+    :syn.group_names(:users)
+  end
+
+  @spec list_connected_tenants(atom()) :: [String.t()]
+  def list_connected_tenants(node) do
+    :syn.group_names(:users, node)
+  end
 end
