94800f3c883a76d36a633cee386e8e203e0c86f9
Chase Granberry <chase@logflare.app>
2023-02-02 13:05:50 -0700

    Start Realtime.Tenants module

--- lib/realtime/tenants.ex
+++ lib/realtime/tenants.ex
@@ -3,6 +3,8 @@ defmodule Realtime.Tenants do
   Everything to do with Tenants.
   """
 
+  alias Realtime.Api.Tenant
+
   @doc """
   Gets a list of connected tenant `external_id` strings in the cluster or a node.
   """
@@ -15,5 +17,45 @@ defmodule Realtime.Tenants do
   @spec list_connected_tenants(atom()) :: [String.t()]
   def list_connected_tenants(node) do
     :syn.group_names(:users, node)
+  end
+
+  @doc """
+  All the keys that we use to create counters and RateLimiters for tenants.
+  """
+
+  @spec limiter_keys(Tenant.t()) :: [
+          {:limit, :all | :channel_joins | :tenant_events | :user_channels, String.t()},
+          ...
+        ]
+  def limiter_keys(%Tenant{} = tenant) do
+    [
+      {:limit, :all, tenant.external_id},
+      {:limit, :user_channels, tenant.external_id},
+      {:limit, :channel_joins, tenant.external_id},
+      {:limit, :tenant_events, tenant.external_id}
+    ]
+  end
+
+  @spec get_tenant_limits(Realtime.Api.Tenant.t(), maybe_improper_list) :: list
+  def get_tenant_limits(%Tenant{} = tenant, keys) when is_list(keys) do
+    nodes = [Node.self() | Node.list()]
+
+    nodes
+    |> Enum.map(fn node ->
+      Task.Supervisor.async({Realtime.TaskSupervisor, node}, fn ->
+        for {_key, name, _external_id} = key <- keys do
+          {_status, response} = Realtime.GenCounter.get(key)
+
+          %{
+            external_id: tenant.external_id,
+            node: node,
+            limiter: name,
+            counter: response
+          }
+        end
+      end)
+    end)
+    |> Task.await_many()
+    |> List.flatten()
   end
 end
---
+++ test/realtime/tenants_test.exs
@@ -0,0 +1,72 @@
+defmodule Realtime.ApiTest do
+  use Realtime.DataCase
+
+  import Mock
+
+  alias Realtime.Api
+  alias Realtime.{Api, RateCounter, GenCounter}
+
+  describe "tenants" do
+    alias Realtime.Api.{Tenant, Extensions}
+    db_conf = Application.compile_env(:realtime, Realtime.Repo)
+
+    @valid_attrs %{
+      external_id: "external_id",
+      name: "localhost",
+      extensions: [
+        %{
+          "type" => "postgres_cdc_rls",
+          "settings" => %{
+            "db_host" => db_conf[:hostname],
+            "db_name" => db_conf[:database],
+            "db_user" => db_conf[:username],
+            "db_password" => db_conf[:password],
+            "db_port" => "5432",
+            "poll_interval" => 100,
+            "poll_max_changes" => 100,
+            "poll_max_record_bytes" => 1_048_576,
+            "region" => "us-east-1"
+          }
+        }
+      ],
+      postgres_cdc_default: "postgres_cdc_rls",
+      jwt_secret: "new secret",
+      max_concurrent_users: 200,
+      max_events_per_second: 100
+    }
+
+    @update_attrs %{
+      external_id: "external_id",
+      jwt_secret: "some updated jwt_secret",
+      name: "some updated name"
+    }
+    @invalid_attrs %{external_id: nil, jwt_secret: nil, name: nil}
+
+    def tenant_fixture(attrs \\ %{}) do
+      {:ok, tenant} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Api.create_tenant()
+
+      tenant
+    end
+
+    test "get_tenant_limits/1" do
+      tenant = tenant_fixture()
+
+      with_mocks([
+        {GenCounter, [], [get: fn _ -> {:ok, 9} end]}
+      ]) do
+        limits = Api.get_tenant_limits(tenant)
+        [all] = Enum.filter(limits, fn e -> e.limiter == :all end)
+        assert all.counter == 9
+        [user_channels] = Enum.filter(limits, fn e -> e.limiter == :user_channels end)
+        assert user_channels.counter == 9
+        [channel_joins] = Enum.filter(limits, fn e -> e.limiter == :channel_joins end)
+        assert channel_joins.counter == 9
+        [tenant_events] = Enum.filter(limits, fn e -> e.limiter == :tenant_events end)
+        assert tenant_events.counter == 9
+      end
+    end
+  end
+end
