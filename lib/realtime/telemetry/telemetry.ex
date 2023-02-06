c1900ec53df8d486d62b3d66ed3cf6b775e7c5a0
Chase Granberry <chase@logflare.app>
2023-02-06 15:17:26 -0700

    feat: release (#479)

--- lib/realtime/telemetry/telemetry.ex
+++ lib/realtime/telemetry/telemetry.ex
@@ -1,4 +1,13 @@
 defmodule Realtime.Telemetry do
+  @moduledoc """
+  Telemetry wrapper
+  """
+
+  @doc """
+  Dispatches Telemetry events.
+  """
+
+  @spec execute([atom, ...], number | map, map) :: :ok
   def execute(event, measurements, metadata \\ %{}) do
     :telemetry.execute(event, measurements, metadata)
   end
