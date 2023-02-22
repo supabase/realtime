c423348be4635ac528f370b4292e146d382497da
abc3 <sts@abc3.dev>
2023-02-22 14:39:20 +0100

    fix: check by batches if pids are alive

--- lib/realtime/helpers.ex
+++ lib/realtime/helpers.ex
@@ -141,6 +141,32 @@ defmodule Realtime.Helpers do
     end
   end
 
+  @doc """
+  Takes the first N items from the queue and returns the list of items and the new queue.
+
+  ## Examples
+
+      iex> q = :queue.new()
+      iex> q = :queue.in(1, q)
+      iex> q = :queue.in(2, q)
+      iex> q = :queue.in(3, q)
+      iex> Realtime.Helpers.queue_take(q, 2)
+      {[2, 1], {[], [3]}}
+  """
+
+  @spec queue_take(:queue.queue(), non_neg_integer()) :: {list(), :queue.queue()}
+  def queue_take(q, count) do
+    Enum.reduce_while(1..count, {[], q}, fn _, {items, queue} ->
+      case :queue.out(queue) do
+        {{:value, item}, new_q} ->
+          {:cont, {[item | items], new_q}}
+
+        {:empty, new_q} ->
+          {:halt, {items, new_q}}
+      end
+    end)
+  end
+
   defp pad(data) do
     to_add = 16 - rem(byte_size(data), 16)
     data <> :binary.copy(<<to_add>>, to_add)
