f4188c6e5651a9013a4fa38479be2aa37df9c93b
Chase Granberry <chase@logflare.app>
2023-02-23 06:47:22 -0700

    feat: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,8 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.6.0",
+      # When bumping this version make sure to check
+      version: "2.7.0",
       elixir: "~> 1.14.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
