bbc239d8c375852b1e6d8361e6db03e35e80b2f4
Chase Granberry <chase@logflare.app>
2023-03-15 14:58:27 -0700

    fix: listen to actual joins event for joins prom metric

--- lib/realtime/monitoring/prom_ex/plugins/tenant.ex
+++ lib/realtime/monitoring/prom_ex/plugins/tenant.ex
@@ -111,14 +111,14 @@ defmodule Realtime.PromEx.Plugins.Tenant do
         ),
         sum(
           [:realtime, :channel, :joins],
-          event_name: [:realtime, :rate_counter, :channel, :events],
+          event_name: [:realtime, :rate_counter, :channel, :joins],
           measurement: :sum,
           description: "Sum of Realtime Channel joins.",
           tags: [:tenant]
         ),
         last_value(
           [:realtime, :channel, :joins, :limit_per_second],
-          event_name: [:realtime, :rate_counter, :channel, :events],
+          event_name: [:realtime, :rate_counter, :channel, :joins],
           measurement: :limit,
           description: "Rate limit of joins per second on a Realtime Channel.",
           tags: [:tenant]
