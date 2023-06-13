85e42cc4400c38541f02d39a68525127ecdd1267
Kazuki Matsuda <mats.kazuki@gmail.com>
2023-06-13 12:36:06 +0900

    fix: update the mix.exs version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.14.0",
+      version: "2.14.1",
       elixir: "~> 1.14.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
