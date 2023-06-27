4fb8a38ec8492b069eb71610692244f942065a10
Chase Granberry <chase@logflare.app>
2023-06-27 07:13:25 -0700

    fix: tailwind build and version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.17.1",
+      version: "2.17.2",
       elixir: "~> 1.14.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -97,7 +97,7 @@ defmodule Realtime.MixProject do
         "run priv/repo/seeds_after_migration.exs",
         "test"
       ],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "tailwind default --minify", "phx.digest"]
     ]
   end
 end
