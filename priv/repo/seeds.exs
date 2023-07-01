631225e1e7754ede1fbe1b43694274be09702562
Chase Granberry <chase@logflare.app>
2023-07-01 08:02:24 -0700

    fix: cleanup seeds stuff

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,13 +1,19 @@
 alias Realtime.{Api.Tenant, Repo}
 import Ecto.Adapters.SQL, only: [query: 3]
 
-tenant_name = "realtime-dev-tenant"
+tenant_name = "localhost"
 
 Repo.transaction(fn ->
   case Repo.get_by(Tenant, external_id: tenant_name) do
     %Tenant{} = tenant -> Repo.delete!(tenant)
     nil -> {:ok, nil}
   end
+
+  # JWT in dev will be
+  # eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE5MDg1MDM0MjUsImlhdCI6MTYyNzg4NjQ0MCwicm9sZSI6InNlcnZpY2Vfcm9sZSJ9.9FzvbdQcM1k_to7XtOJcu_WXHaTQ7wlWAGafhf7VK8Y
+
+  # Subscribe to fake tenant table with the Inspector
+  # http://localhost:4000/inspector/new?bearer=&channel=any&host=http%3A%2F%2Frealtime-dev-tenant.localhost.localhost%3A4000&log_level=info&schema=%2A&table=%2A&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE5MDg1MDM0MjUsImlhdCI6MTYyNzg4NjQ0MCwicm9sZSI6InNlcnZpY2Vfcm9sZSJ9.9FzvbdQcM1k_to7XtOJcu_WXHaTQ7wlWAGafhf7VK8Y
 
   %Tenant{}
   |> Tenant.changeset(%{
@@ -39,15 +45,43 @@ publication = "supabase_realtime"
 {:ok, _} =
   Repo.transaction(fn ->
     [
-      "drop publication if exists #{publication}",
-      "drop table if exists public.test_tenant;",
+      # drop first
+      "drop   publication if exists #{publication}",
+      "drop   table if exists public.test_tenant",
+      "revoke usage on schema public from anon, authenticated, service_role;",
+      "alter default privileges in schema public revoke all on tables from anon, authenticated, service_role;",
+      "alter default privileges in schema public revoke all on functions from anon, authenticated, service_role;",
+      "alter default privileges in schema public revoke all on sequences from anon, authenticated, service_role;",
+      "drop role if exists anon",
+      "drop role if exists authenticated",
+      "drop role if exists service_role",
+
+      # roles
+      "create role anon          nologin noinherit",
+      "create role authenticated nologin noinherit",
+      "create role service_role  nologin noinherit bypassrls",
+
+      # schema
+      "create schema if not exists realtime",
+
+      # example tenant tables
       "create table public.test_tenant (
         id SERIAL PRIMARY KEY,
         details text
         );",
-      "grant all on table public.test_tenant to anon;",
-      "grant all on table public.test_tenant to postgres;",
-      "grant all on table public.test_tenant to authenticated;",
+
+      # role grants
+      "grant all   on table  public.test_tenant to anon",
+      "grant all   on table  public.test_tenant to postgres",
+      "grant all   on table  public.test_tenant to authenticated",
+      "grant usage on schema public             to anon, authenticated, service_role",
+
+      # priviledges
+      "alter default privileges in schema public grant all on tables    to anon, authenticated, service_role",
+      "alter default privileges in schema public grant all on functions to anon, authenticated, service_role",
+      "alter default privileges in schema public grant all on sequences to anon, authenticated, service_role",
+
+      # publication
       "create publication #{publication} for table public.test_tenant"
     ]
     |> Enum.each(&query(Repo, &1, []))
--- priv/repo/seeds_before_migration.exs
+++
@@ -1,5 +0,0 @@
-import Ecto.Adapters.SQL, only: [query: 3]
-
-[
-  "create schema if not exists realtime"
-] |> Enum.each(&query(Realtime.Repo, &1, []))
