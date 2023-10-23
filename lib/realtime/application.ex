c3ed74bd1fa43569c24439f1b132a529109eada8
Filipe Cabaço <filipecabaco@gmail.com>
2023-10-23 11:36:11 +0100

    fix - Set slot_name_suffix to avoid conflicts on deploys
    Sets a default value for slot_name_suffix so we avoid issues during deployments
    
    Plus sets the SynHandler as conflict resolution for :syn on the Application.ex level

--- lib/realtime/application.ex
+++ lib/realtime/application.ex
@@ -42,6 +42,7 @@ defmodule Realtime.Application do
       name: Realtime.Registry.Unique
     )
 
+    :syn.set_event_handler(Realtime.SynHandler)
     :ok = :syn.add_node_to_scopes([Realtime.Tenants.Connect])
     :ok = :syn.add_node_to_scopes([:users, RegionNodes])
     region = Application.get_env(:realtime, :region)
