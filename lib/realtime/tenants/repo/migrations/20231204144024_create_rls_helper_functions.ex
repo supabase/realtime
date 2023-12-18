8a693d034a516db61edf293d53b4f8ded6d9592c
Filipe Cabaço <filipecabaco@gmail.com>
2023-12-18 17:06:45 +0000

    create auth functions if required only

--- lib/realtime/tenants/repo/migrations/20231204144024_create_rls_helper_functions.ex
+++ lib/realtime/tenants/repo/migrations/20231204144024_create_rls_helper_functions.ex
@@ -10,16 +10,28 @@ defmodule Realtime.Tenants.Migrations.CreateRlsHelperFunctions do
     $$ language sql stable;
     """)
 
+    execute("create schema if not exists auth")
+
     execute("""
-    create or replace function auth.role() returns text as $$
-        select nullif(current_setting('request.jwt.claim.role', true), '')::text;
-      $$ language sql stable;
+    do $func_exists$
+    begin if not exists(select * from pg_proc where proname = 'role')
+    then
+      create or replace function auth.uid() returns uuid as $function_def$
+        select nullif(current_setting('request.jwt.claim.sub', true), '')::uuid;
+      $function_def$ language sql stable;
+    end if;
+    end $func_exists$;
     """)
 
     execute("""
-    create or replace function auth.role() returns text as $$
-         select nullif(current_setting('request.jwt.claim.role', true), '')::text;
-       $$ language sql stable;
+    do $func_exists$
+    begin if not exists(select * from pg_proc where proname = 'role')
+    then
+      create function auth.role() returns text as $function_def$
+        select nullif(current_setting('request.jwt.claim.role', true), '')::text;
+      $function_def$ language sql stable;
+    end if;
+    end $func_exists$;
     """)
   end
 end
