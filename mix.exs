73bda1d11ab5652009176476556313a5765967f4
Filipe Cabaço <filipe@supabase.io>
2023-12-19 23:11:35 +0000

    fix: Bump up version (#763)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.25.43",
+      version: "2.25.44",
       elixir: "~> 1.14.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
