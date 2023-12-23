1a1ccba3bc48dd1cf98a00a5dd389f200a3c9779
Wen Bo Xie <5532241+w3b6x9@users.noreply.github.com>
2023-12-23 04:14:00 -0800

    fix: add default region nodes when no direct region mapping (#767)
    * fix: add default region nodes when no direct region mapping
    
    * feat: support aws ap-east-1 tenant region mapping

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.25.47",
+      version: "2.25.48",
       elixir: "~> 1.14.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
