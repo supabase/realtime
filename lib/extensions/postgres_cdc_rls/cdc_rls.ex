434d2a05dd421a5b56eef485a96f5c40eb51b327
Chase Granberry <chase@logflare.app>
2024-04-16 05:45:09 -0700

    fix: tune connections a bit more (#850)

--- lib/extensions/postgres_cdc_rls/cdc_rls.ex
+++ lib/extensions/postgres_cdc_rls/cdc_rls.ex
@@ -96,7 +96,7 @@ defmodule Extensions.PostgresCdcRls do
 
   @spec start(map()) :: :ok | {:error, :already_started | :reserved}
   def start(%{"id" => tenant} = args) when is_binary(tenant) do
-    args = Map.merge(args, %{"subs_pool_size" => Map.get(args, "subcriber_pool_size", 5)})
+    args = Map.merge(args, %{"subs_pool_size" => Map.get(args, "subcriber_pool_size", 3)})
 
     Logger.debug("Starting #{__MODULE__} extension with args: #{inspect(args, pretty: true)}")
 
