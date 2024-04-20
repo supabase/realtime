d7eaeb0223b0c0ab22350edf7e8ea44719eefa6f
Filipe Cabaço <filipecabaco@gmail.com>
2024-04-20 09:06:26 +0100

    fix: Public Channel definition

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.28.31",
+      version: "2.28.32",
       elixir: "~> 1.14.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
