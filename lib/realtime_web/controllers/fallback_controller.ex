4681606f1720dfd9e8f652bc919346691ca99793
Chase Granberry <chase@logflare.app>
2024-07-15 11:06:50 -0700

    fix: log unknown errors in FallbackController

--- lib/realtime_web/controllers/fallback_controller.ex
+++ lib/realtime_web/controllers/fallback_controller.ex
@@ -8,18 +8,6 @@ defmodule RealtimeWeb.FallbackController do
   import RealtimeWeb.ErrorHelpers
   alias Realtime.Helpers
 
-  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
-    Helpers.log_error(
-      "UnprocessableEntity",
-      Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
-    )
-
-    conn
-    |> put_status(:unprocessable_entity)
-    |> put_view(RealtimeWeb.ChangesetView)
-    |> render("error.json", changeset: changeset)
-  end
-
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
@@ -43,6 +31,18 @@ defmodule RealtimeWeb.FallbackController do
     |> render("error.json", message: message)
   end
 
+  def call(conn, %Ecto.Changeset{valid?: true} = changeset) do
+    Helpers.log_error(
+      "UnprocessableEntity",
+      Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
+    )
+
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(RealtimeWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
   def call(conn, %Ecto.Changeset{valid?: false} = changeset) do
     Helpers.log_error(
       "UnprocessableEntity",
@@ -53,5 +53,14 @@ defmodule RealtimeWeb.FallbackController do
     |> put_status(:unprocessable_entity)
     |> put_view(RealtimeWeb.ChangesetView)
     |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, response) do
+    Helpers.log_error("UnknownError",response)
+
+    conn
+    |> put_status(:not_found)
+    |> put_view(RealtimeWeb.ErrorView)
+    |> render("error.json", message: "Unknown error")
   end
 end
