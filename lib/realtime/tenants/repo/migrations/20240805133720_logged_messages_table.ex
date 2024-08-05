0ef2af488211e75439b95415e12dbd145821441a
Filipe Cabaço <filipecabaco@gmail.com>
2024-08-05 13:42:08 +0100

    fix: Use logged tables for realtime.messages

---
+++ lib/realtime/tenants/repo/migrations/20240805133720_logged_messages_table.ex
@@ -0,0 +1,10 @@
+defmodule Realtime.Tenants.Migrations.LoggedMessagesTable do
+  @moduledoc false
+  use Ecto.Migration
+
+  def change do
+    execute """
+    ALTER TABLE realtime.messages SET LOGGED;
+    """
+  end
+end
