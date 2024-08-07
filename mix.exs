34be08e17617d5fa5f51b9ababa2122bfe058d67
Filipe Cabaço <filipecabaco@gmail.com>
2024-08-07 14:50:42 +0100

    fix: Add tests to RPC module

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.30.25",
+      version: "2.30.26",
       elixir: "~> 1.16.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
