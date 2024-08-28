f3a03eb3c7db9da297f71c836210393b56de4c9e
Filipe Cabaço <filipecabaco@gmail.com>
2024-08-28 17:37:33 +0100

    fix: Increase interval on workerjs

--- priv/static/worker.js
+++ priv/static/worker.js
@@ -1,5 +1,5 @@
 addEventListener("message", (e) => {
   if (e.data === "start") {
-    setInterval(() => postMessage("keepAlive"), 10000);
+    setInterval(() => postMessage("keepAlive"), 30000);
   }
 });
