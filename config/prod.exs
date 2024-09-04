b4108c5a6eb90c4fc749c62e520119fc604e4911
Stas <sts@abc3.dev>
2024-09-04 13:21:36 +0200

    feat: add region metadata to logger configuration

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ import Config
 # Do not print debug messages in production
 config :logger, :warning,
   format: "$time [$level] $message $metadata\n",
-  metadata: [:error_code, :file, :pid, :project, :external_id, :application_name]
+  metadata: [:error_code, :file, :pid, :project, :external_id, :application_name, :region]
 
 # ## SSL Support
 #
