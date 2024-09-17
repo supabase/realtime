a95a7903e429566dbf611328c34cdee1a3bd2a8d
Filipe Cabaço <filipecabaco@gmail.com>
2024-09-17 19:09:13 +0100

    feat: Add broadcast functionality from triggers
    Adds a new functionality to broadcast db changes. It adds:
    * New function to broadcast from postgres using triggers

---
+++ lib/realtime/tenants/repo/migrations/20240917170412_add_payload_to_messages.ex
@@ -0,0 +1,59 @@
+defmodule Realtime.Tenants.Migrations.AddPayloadToMessages do
+  @moduledoc false
+  use Ecto.Migration
+
+  def change do
+    alter table(:messages) do
+      add_if_not_exists :payload, :map
+      add_if_not_exists :event, :text
+      add_if_not_exists :topic, :text
+      add_if_not_exists :private, :boolean, default: true
+      modify :inserted_at, :utc_datetime, default: fragment("now()")
+      modify :updated_at, :utc_datetime, default: fragment("now()")
+    end
+
+    execute """
+    CREATE OR REPLACE FUNCTION realtime.send(payload jsonb, event text, topic text, private boolean DEFAULT true, extension text DEFAULT 'broadcast')
+    RETURNS void
+    AS $$
+    BEGIN
+        INSERT INTO realtime.messages (payload, event, topic, extension, private)
+        VALUES (payload, event, topic, extension, private);
+    END;
+    $$
+    LANGUAGE plpgsql;
+    """
+
+    execute """
+    CREATE OR REPLACE FUNCTION realtime.broadcast_change ()
+        RETURNS TRIGGER
+        AS $$
+    DECLARE
+        -- Declare a variable to hold the JSONB representation of the row
+        row_data jsonb := '{}'::jsonb;
+        -- Declare entry that will be written to the realtime.messages table
+        topic_name text := TG_ARGV[0]::text;
+        event_name text := COALESCE(TG_ARGV[1]::text, TG_TABLE_SCHEMA || '.' || TG_TABLE_NAME);
+    BEGIN
+        -- Ensure topic_name is provided
+        IF topic_name IS NULL THEN
+            RAISE EXCEPTION 'Topic name must be provided';
+        END IF;
+        -- Check the operation type and handle accordingly
+        IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' OR TG_OP = 'DELETE' THEN
+            row_data := jsonb_build_object('old_record', OLD, 'record', NEW, 'operation', TG_OP, 'table', TG_TABLE_NAME, 'schema', TG_TABLE_SCHEMA);
+            PERFORM realtime.send (row_data, event_name, topic_name);
+            RETURN NULL;
+        ELSE
+            RAISE EXCEPTION 'Unexpected operation type: %', TG_OP;
+        END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE EXCEPTION 'Failed to process the row: %', SQLERRM;
+    END;
+
+    $$
+    LANGUAGE plpgsql;
+    """
+  end
+end
