38c8ba1b6de625d1dc7ed562700e29c528fd1bcf
Filipe Cabaço <filipe@supabase.io>
2024-09-24 13:07:23 +0100

    Update lib/realtime/tenants/repo/migrations/20240919140541_reduce_grants_postgres_user.ex
    Co-authored-by: Bobbie Soedirgo <31685197+soedirgo@users.noreply.github.com>

--- lib/realtime/tenants/repo/migrations/20240919140541_reduce_grants_postgres_user.ex
+++ lib/realtime/tenants/repo/migrations/20240919140541_reduce_grants_postgres_user.ex
@@ -4,6 +4,9 @@ defmodule Realtime.Tenants.Migrations.ReduceGrantsPostgresUser do
 
   def change do
     execute("revoke supabase_realtime_admin from postgres")
+    execute("alter default privileges for role supabase_admin in schema realtime revoke all on tables from postgres")
+    execute("alter default privileges for role supabase_admin in schema realtime revoke all on functions from postgres")
+    execute("alter default privileges for role supabase_admin in schema realtime revoke all on sequences from postgres")
 
     execute("revoke all on table realtime.schema_migrations from postgres, anon, authenticated, service_role")
     execute("grant select on table realtime.schema_migrations to postgres with grant option")
