3878e86430be21e4e91c58ebe78e39d01d6eb962
Filipe Cabaço <filipe@supabase.io>
2024-09-24 16:26:29 +0100

    Update lib/realtime/tenants/repo/migrations/20240919140541_reduce_grants_postgres_user.ex
    Co-authored-by: Bobbie Soedirgo <31685197+soedirgo@users.noreply.github.com>

--- lib/realtime/tenants/repo/migrations/20240919140541_reduce_grants_postgres_user.ex
+++ lib/realtime/tenants/repo/migrations/20240919140541_reduce_grants_postgres_user.ex
@@ -11,8 +11,8 @@ defmodule Realtime.Tenants.Migrations.ReduceGrantsPostgresUser do
     execute("revoke all on table realtime.schema_migrations from postgres, anon, authenticated, service_role")
     execute("grant select on table realtime.schema_migrations to postgres with grant option")
 
-    execute("revoke all on table realtime.messages from postgres")
-    execute("grant select on table realtime.messages to postgres with grant option")
+    execute("revoke all on table realtime.messages from postgres, anon, authenticated, service_role")
+    execute("grant select, insert on table realtime.messages to postgres with grant option")
 
     execute("revoke all on table realtime.subscription from postgres")
     execute("grant select on table realtime.subscription to postgres with grant option")
