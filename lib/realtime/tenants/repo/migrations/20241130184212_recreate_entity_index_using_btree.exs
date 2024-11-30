3ac7aa1dbecd2cc794bb0f5991941d4c8791cd91
Wen Bo Xie <wenbo.xie3@gmail.com>
2024-11-30 14:27:06 -0500

    create index concurrently

--- lib/realtime/tenants/repo/migrations/20241130184212_recreate_entity_index_using_btree.exs
+++ lib/realtime/tenants/repo/migrations/20241130184212_recreate_entity_index_using_btree.exs
@@ -6,8 +6,14 @@ defmodule Realtime.Tenants.Migrations.RecreateEntityIndexUsingBtree do
   def change do
     execute("drop index if exists \"realtime\".\"ix_realtime_subscription_entity\"")
 
-    execute(
-      "create index if not exists ix_realtime_subscription_entity on realtime.subscription using btree (entity)"
-    )
+    execute("""
+    do $$
+    begin
+      create index concurrently if not exists ix_realtime_subscription_entity on realtime.subscription using btree (entity);
+    exception
+      when others then
+        create index if not exists ix_realtime_subscription_entity on realtime.subscription using btree (entity);
+    end$$;
+    """)
   end
 end
