a49619e8d3eeb12947bfbc68975ae1d7243e601d
Filipe Cabaço <filipe@supabase.io>
2025-01-28 23:20:17 +0000

    fix: Send functions sets topic properly (#1286)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.34.11",
+      version: "2.34.12",
       elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
