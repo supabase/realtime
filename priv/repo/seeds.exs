6effe4146686ac06f8458aadcfcf18d02fa451c1
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-05-07 14:02:52 +1200

    fix: Also reset tenant database on seeds.exs
    Given that we are deleting and re-creating the tenant it seems natural
    to do the same for the tenant database as we are losing the
    "migrations_ran" information

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,45 +5,59 @@ import Ecto.Adapters.SQL, only: [query: 3]
 alias Realtime.Api.Tenant
 alias Realtime.Repo
 alias Realtime.Tenants
+alias Realtime.Database
 
 tenant_name = System.get_env("SELF_HOST_TENANT_NAME", "realtime-dev")
 env = if :ets.whereis(Mix.State) != :undefined, do: Mix.env(), else: :prod
 default_db_host = if env in [:dev, :test], do: "127.0.0.1", else: "host.docker.internal"
 
-Repo.transaction(fn ->
-  case Repo.get_by(Tenant, external_id: tenant_name) do
-    %Tenant{} = tenant -> Repo.delete!(tenant)
-    nil -> {:ok, nil}
-  end
+{:ok, tenant} =
+  Repo.transaction(fn ->
+    case Repo.get_by(Tenant, external_id: tenant_name) do
+      %Tenant{} = tenant -> Repo.delete!(tenant)
+      nil -> {:ok, nil}
+    end
 
-  %Tenant{}
-  |> Tenant.changeset(%{
-    "name" => tenant_name,
-    "external_id" => tenant_name,
-    "jwt_secret" => System.get_env("API_JWT_SECRET", "super-secret-jwt-token-with-at-least-32-characters-long"),
-    "jwt_jwks" => System.get_env("API_JWT_JWKS") |> then(fn v -> if v, do: Jason.decode!(v) end),
-    "extensions" => [
-      %{
-        "type" => "postgres_cdc_rls",
-        "settings" => %{
-          "db_name" => System.get_env("DB_NAME", "postgres"),
-          "db_host" => System.get_env("DB_HOST", default_db_host),
-          "db_user" => System.get_env("DB_USER", "supabase_admin"),
-          "db_password" => System.get_env("DB_PASSWORD", "postgres"),
-          "db_port" => System.get_env("DB_PORT", "5433"),
-          "region" => "us-east-1",
-          "poll_interval_ms" => 100,
-          "poll_max_record_bytes" => 1_048_576,
-          "ssl_enforced" => false
+    %Tenant{}
+    |> Tenant.changeset(%{
+      "name" => tenant_name,
+      "external_id" => tenant_name,
+      "jwt_secret" => System.get_env("API_JWT_SECRET", "super-secret-jwt-token-with-at-least-32-characters-long"),
+      "jwt_jwks" => System.get_env("API_JWT_JWKS") |> then(fn v -> if v, do: Jason.decode!(v) end),
+      "extensions" => [
+        %{
+          "type" => "postgres_cdc_rls",
+          "settings" => %{
+            "db_name" => System.get_env("DB_NAME", "postgres"),
+            "db_host" => System.get_env("DB_HOST", default_db_host),
+            "db_user" => System.get_env("DB_USER", "supabase_admin"),
+            "db_password" => System.get_env("DB_PASSWORD", "postgres"),
+            "db_port" => System.get_env("DB_PORT", "5433"),
+            "region" => "us-east-1",
+            "poll_interval_ms" => 100,
+            "poll_max_record_bytes" => 1_048_576,
+            "ssl_enforced" => false
+          }
         }
-      }
-    ]
-  })
-  |> Repo.insert!()
+      ]
+    })
+    |> Repo.insert!()
+  end)
 
-  tenant = Tenants.get_tenant_by_external_id(tenant_name)
-  Tenants.Migrations.run_migrations(tenant)
+settings = Database.from_tenant(tenant, "realtime_migrations", :stop)
+settings = %{settings | max_restarts: 0, ssl: false}
+{:ok, tenant_conn} = Database.connect_db(settings)
+
+Postgrex.transaction(tenant_conn, fn db_conn ->
+  Postgrex.query!(db_conn, "DROP SCHEMA IF EXISTS realtime CASCADE", [])
+  Postgrex.query!(db_conn, "CREATE SCHEMA IF NOT EXISTS realtime", [])
 end)
+
+case Tenants.Migrations.run_migrations(tenant) do
+  :ok -> :ok
+  :noop -> :ok
+  _ -> raise "Running Migrations failed"
+end
 
 if env in [:dev, :test] do
   publication = "supabase_realtime"
