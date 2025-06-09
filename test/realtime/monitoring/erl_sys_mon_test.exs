b2a66753a95eba09c59cd6b955b84f3c12b4f6a2
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-06-09 12:12:42 +1200

    fix: ErlSysMon tests

--- test/realtime/monitoring/erl_sys_mon_test.exs
+++ test/realtime/monitoring/erl_sys_mon_test.exs
@@ -8,7 +8,11 @@ defmodule Realtime.Monitoring.ErlSysMonTest do
       start_supervised!({ErlSysMon, config: [{:long_message_queue, {1, 10}}]})
 
       assert capture_log(fn ->
-               Task.async(fn -> Enum.map(1..100_000, &send(self(), &1)) end)
+               Task.async(fn ->
+                 Enum.map(1..1000, &send(self(), &1))
+                 # Wait  for ErlSysMon to notice
+                 Process.sleep(4000)
+               end)
                |> Task.await()
              end) =~ "Realtime.ErlSysMon message:"
     end
