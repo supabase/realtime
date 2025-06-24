b04ff67062c10d060d7d1e9eb4624c4af211921a
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-06-24 13:58:39 +1200

    fix: use TenantBroadcaster on BatchBroadcast
    This way gen_rpc can be used for API and realtime.messages broadcasting

--- test/realtime_web/controllers/broadcast_controller_test.exs
+++ test/realtime_web/controllers/broadcast_controller_test.exs
@@ -26,82 +26,103 @@ defmodule RealtimeWeb.BroadcastControllerTest do
     {:ok, conn: conn, tenant: tenant}
   end
 
-  describe "broadcast" do
-    test "returns 202 when batch of messages is broadcasted", %{conn: conn, tenant: tenant} do
-      broadcast_events_key = Tenants.events_per_second_key(tenant)
-      request_events_key = Tenants.requests_per_second_key(tenant)
+  for adapter <- [:phoenix, :gen_rpc] do
+    describe "broadcast #{adapter}" do
+      @describetag adapter: adapter
 
-      GenCounter
-      |> expect(:add, fn ^request_events_key -> :ok end)
-      |> expect(:add, 2, fn ^broadcast_events_key -> :ok end)
+      setup %{tenant: tenant, adapter: broadcast_adapter} do
+        {:ok, tenant} = Realtime.Api.update_tenant(tenant, %{broadcast_adapter: broadcast_adapter})
+        # Warm cache to avoid Cachex and Ecto.Sandbox ownership issues
+        Cachex.put!(Realtime.Tenants.Cache, {{:get_tenant_by_external_id, 1}, [tenant.external_id]}, {:cached, tenant})
+        %{tenant: tenant}
+      end
 
-      sub_topic_1 = "sub_topic_1"
-      sub_topic_2 = "sub_topic_2"
-      topic_1 = Tenants.tenant_topic(tenant, sub_topic_1)
-      topic_2 = Tenants.tenant_topic(tenant, sub_topic_2)
+      test "returns 202 when batch of messages is broadcasted", %{conn: conn, tenant: tenant} do
+        broadcast_events_key = Tenants.events_per_second_key(tenant)
+        request_events_key = Tenants.requests_per_second_key(tenant)
 
-      payload_1 = %{"data" => "data"}
-      payload_2 = %{"data" => "data"}
-      event_1 = "event_1"
-      event_2 = "event_2"
+        GenCounter
+        |> expect(:add, fn ^request_events_key -> :ok end)
+        |> expect(:add, 2, fn ^broadcast_events_key -> :ok end)
 
-      payload_topic_1 = %{"payload" => payload_1, "event" => event_1, "type" => "broadcast"}
-      expect(Endpoint, :broadcast_from, 2, fn _, ^topic_1, "broadcast", ^payload_topic_1 -> :ok end)
+        sub_topic_1 = "sub_topic_1"
+        sub_topic_2 = "sub_topic_2"
+        topic_1 = Tenants.tenant_topic(tenant, sub_topic_1)
+        topic_2 = Tenants.tenant_topic(tenant, sub_topic_2)
 
-      payload_topic_2 = %{"payload" => payload_2, "event" => event_2, "type" => "broadcast"}
-      expect(Endpoint, :broadcast_from, fn _, ^topic_2, "broadcast", ^payload_topic_2 -> :ok end)
+        payload_1 = %{"data" => "data"}
+        payload_2 = %{"data" => "data"}
+        event_1 = "event_1"
+        event_2 = "event_2"
 
-      conn =
-        post(conn, Routes.broadcast_path(conn, :broadcast), %{
-          "messages" => [
-            %{"topic" => sub_topic_1, "payload" => payload_1, "event" => event_1},
-            %{"topic" => sub_topic_1, "payload" => payload_1, "event" => event_1},
-            %{"topic" => sub_topic_2, "payload" => payload_2, "event" => event_2}
-          ]
-        })
+        payload_topic_1 = %{"payload" => payload_1, "event" => event_1, "type" => "broadcast"}
 
-      assert conn.status == 202
-    end
+        payload_topic_2 = %{"payload" => payload_2, "event" => event_2, "type" => "broadcast"}
 
-    test "returns 422 when batch of messages includes badly formed messages", %{conn: conn, tenant: tenant} do
-      reject(&Endpoint.broadcast_from/4)
+        Endpoint.subscribe(topic_1)
+        Endpoint.subscribe(topic_2)
 
-      conn =
-        post(conn, Routes.broadcast_path(conn, :broadcast), %{
-          "messages" => [
-            %{
-              "topic" => "topic"
-            },
-            %{
-              "payload" => %{"data" => "data"}
-            },
-            %{
-              "topic" => "topic",
-              "payload" => %{"data" => "data"},
-              "event" => "event"
-            }
-          ]
-        })
+        conn =
+          post(conn, Routes.broadcast_path(conn, :broadcast), %{
+            "messages" => [
+              %{"topic" => sub_topic_1, "payload" => payload_1, "event" => event_1},
+              %{"topic" => sub_topic_1, "payload" => payload_1, "event" => event_1},
+              %{"topic" => sub_topic_2, "payload" => payload_2, "event" => event_2}
+            ]
+          })
 
-      assert Jason.decode!(conn.resp_body) == %{
-               "errors" => %{
-                 "messages" => [
-                   %{"payload" => ["can't be blank"], "event" => ["can't be blank"]},
-                   %{"topic" => ["can't be blank"], "event" => ["can't be blank"]},
-                   %{}
-                 ]
+        assert conn.status == 202
+
+        assert_receive %Phoenix.Socket.Broadcast{topic: ^topic_1, event: "broadcast", payload: ^payload_topic_1}
+        assert_receive %Phoenix.Socket.Broadcast{topic: ^topic_1, event: "broadcast", payload: ^payload_topic_1}
+        assert_receive %Phoenix.Socket.Broadcast{topic: ^topic_2, event: "broadcast", payload: ^payload_topic_2}
+        refute_receive %Phoenix.Socket.Broadcast{}
+      end
+
+      test "returns 422 when batch of messages includes badly formed messages", %{conn: conn, tenant: tenant} do
+        topic = Tenants.tenant_topic(tenant, "topic")
+
+        Endpoint.subscribe(topic)
+
+        conn =
+          post(conn, Routes.broadcast_path(conn, :broadcast), %{
+            "messages" => [
+              %{
+                "topic" => "topic"
+              },
+              %{
+                "payload" => %{"data" => "data"}
+              },
+              %{
+                "topic" => "topic",
+                "payload" => %{"data" => "data"},
+                "event" => "event"
+              }
+            ]
+          })
+
+        assert Jason.decode!(conn.resp_body) == %{
+                 "errors" => %{
+                   "messages" => [
+                     %{"payload" => ["can't be blank"], "event" => ["can't be blank"]},
+                     %{"topic" => ["can't be blank"], "event" => ["can't be blank"]},
+                     %{}
+                   ]
+                 }
                }
-             }
 
-      assert conn.status == 422
+        assert conn.status == 422
 
-      # Wait for counters to increment
-      Process.sleep(1000)
-      {:ok, rate_counter} = RateCounter.get(Tenants.requests_per_second_key(tenant))
-      assert rate_counter.avg != 0.0
+        # Wait for counters to increment
+        Process.sleep(1000)
+        {:ok, rate_counter} = RateCounter.get(Tenants.requests_per_second_key(tenant))
+        assert rate_counter.avg != 0.0
 
-      {:ok, rate_counter} = RateCounter.get(Tenants.events_per_second_key(tenant))
-      assert rate_counter.avg == 0.0
+        {:ok, rate_counter} = RateCounter.get(Tenants.events_per_second_key(tenant))
+        assert rate_counter.avg == 0.0
+
+        refute_receive %Phoenix.Socket.Broadcast{}
+      end
     end
   end
 
