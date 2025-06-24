a8a1555f802dadb5616c47920dec304afdeaa672
Eduardo Gurgel <eduardo.gurgel@supabase.io>
2025-06-24 15:46:53 +1200

    fix: change TenantBroadcaster.GenRpc to use topic as a key (#1427)
    * fix: change TenantBroadcaster.GenRpc to use topic as a key
    
    This ensures that two messages sent to the same topic from the same node
    are sent one after the other instead of in parallel
    
    * fix: broadcast controller tests

--- lib/realtime_web/tenant_broadcaster/gen_rpc.ex
+++ lib/realtime_web/tenant_broadcaster/gen_rpc.ex
@@ -5,13 +5,13 @@ defmodule RealtimeWeb.TenantBroadcaster.GenRpc do
 
   @impl true
   def broadcast(topic, event, msg) do
-    Realtime.GenRpc.multicast(RealtimeWeb.Endpoint, :local_broadcast, [topic, event, msg])
+    Realtime.GenRpc.multicast(RealtimeWeb.Endpoint, :local_broadcast, [topic, event, msg], key: topic)
     :ok
   end
 
   @impl true
   def broadcast_from(from, topic, event, msg) do
-    Realtime.GenRpc.multicast(RealtimeWeb.Endpoint, :local_broadcast_from, [from, topic, event, msg])
+    Realtime.GenRpc.multicast(RealtimeWeb.Endpoint, :local_broadcast_from, [from, topic, event, msg], key: topic)
     :ok
   end
 end
