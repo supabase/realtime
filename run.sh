a8bfc41f53d9c848932905dd1f253e964cb7f278
Div Arora <darora@users.noreply.github.com>
2025-09-01 17:30:31 +0800

    feat: add support for generating certs for securing inter-node comms (#1516)
    This is anticipated to be a temporary feature that will be deprecated
    and removed once we move to a platform that offers easy init container
    support.

--- run.sh
+++ run.sh
@@ -55,8 +55,43 @@ upload_crash_dump_to_s3() {
     exit "$EXIT_CODE"
 }
 
+generate_certs() {
+    aws secretsmanager get-secret-value --secret-id "${CLUSTER_SECRET_ID}" --region "${CLUSTER_SECRET_REGION}" | jq -r '.SecretString' > cert_secrets
+    jq -r '.key' cert_secrets | base64 -d > ca.key
+    jq -r '.cert' cert_secrets | base64 -d > ca.cert
+    openssl req -new -nodes -out server.csr -keyout server.key -subj "/C=US/ST=Delaware/L=New Castle/O=Supabase Inc/CN=$(hostname -f)"
+    openssl x509 -req -in server.csr -days 90 -CA ca.cert -CAkey ca.key -out server.crt
+    rm -f ca.key
+    CWD=`pwd`
+    export GEN_RPC_CACERTFILE="$CWD/ca.cert"
+    export GEN_RPC_KEYFILE="$CWD/server.key"
+    export GEN_RPC_CERTFILE="$CWD/server.cert"
+    chmod a+r "$GEN_RPC_CACERTFILE"
+    chmod a+r "$GEN_RPC_KEYFILE"
+    chmod a+r "$GEN_RPC_CERTFILE"
+    cat > inet_tls.conf <<EOF
+[
+  {server, [
+    {certfile, "${GEN_RPC_CERTFILE}"},
+    {keyfile, "${GEN_RPC_KEYFILE}"},
+    {secure_renegotiate, true}
+  ]},
+  {client, [
+    {cacertfile, "${GEN_RPC_CACERTFILE}"},
+    {verify, verify_none},
+    {secure_renegotiate, true}
+  ]}
+].
+EOF
+    export ERL_AFLAGS="${ERL_AFLAGS} -proto_dist inet_tls -ssl_dist_optfile ${CWD}/inet_tls.conf"
+}
+
 if [ "${ENABLE_ERL_CRASH_DUMP:-false}" = true ]; then
     trap upload_crash_dump_to_s3 INT TERM KILL EXIT
+fi
+
+if [[ -n "${GENERATE_CLUSTER_CERTS}" ]] ; then
+    generate_certs()
 fi
 
 echo "Running migrations"
