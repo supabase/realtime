e8895cdfc8699866c91fbf258a1a2aae6ba4bf73
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-09-18 14:50:37 +1200

    cursor

---
+++ multiplayer/components/realtime-cursors.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Cursor } from '@/components/cursor'
+import { useRealtimeCursors } from '@/hooks/use-realtime-cursors'
+
+const THROTTLE_MS = 50
+
+export const RealtimeCursors = ({ roomName, username }: { roomName: string; username: string }) => {
+  const { cursors } = useRealtimeCursors({ roomName, username, throttleMs: THROTTLE_MS })
+
+  return (
+    <div>
+      {Object.keys(cursors).map((id) => (
+        <Cursor
+          key={id}
+          className="fixed transition-transform ease-in-out z-50"
+          style={{
+            transitionDuration: '100ms',
+            top: 0,
+            left: 0,
+            transform: `translate(${cursors[id].position.x}px, ${cursors[id].position.y}px)`,
+          }}
+          color={cursors[id].color}
+          name={cursors[id].user.name}
+        />
+      ))}
+    </div>
+  )
+}
