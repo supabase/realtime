4f44873e781e7c24c2abeeb472606c46b643e0b4
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-09-18 14:50:37 +1200

    cursor

--- multiplayer/app/page.tsx
+++ multiplayer/app/page.tsx
@@ -1,37 +1,29 @@
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import Link from "next/link";
+import { RealtimeCursors } from '@/components/realtime-cursors'
+import { nanoid } from 'nanoid'
+
+// Generate a random user id
+const userId = nanoid()
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-            </div>
-          </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-          </main>
+      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center space-x-2 pointer-events-none">
+        <div className="flex items-center justify-center space-x-2 border border-scale-1200 rounded-md px-3 py-2 opacity-20">
+          <p className="text-scale-1200 cursor-default text-sm">Chat</p>
+          <code className="bg-scale-1100 text-scale-100 px-1 h-6 rounded flex items-center justify-center">
+            ↩
+          </code>
         </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
+        <div className="flex items-center justify-center space-x-2 border border-scale-1200 rounded-md px-3 py-2 opacity-20">
+          <p className="text-scale-1200 cursor-default text-sm">Escape</p>
+          <code className="bg-scale-1100 text-scale-100 px-1 h-6 rounded flex items-center justify-center text-xs">
+            ESC
+          </code>
+        </div>
       </div>
+      <RealtimeCursors roomName="macrodata_refinement_office" username="Mark Scout" />
     </main>
   );
 }
