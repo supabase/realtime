c50cc1c17163f65145f18e945632a8d4a9ed6b1e
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-11-03 14:24:36 +1300

    chore: increase timeout for socket monitor message

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.60.0",
+      version: "2.61.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
