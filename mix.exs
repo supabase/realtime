eb5ed53fab1e49a59be79a8c904b905709cd8beb
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-11-09 22:08:49 +1300

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.62.0",
+      version: "2.63.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
