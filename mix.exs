a01ca3083861bb9767dccc3b417c4d87b92fb040
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-11-13 15:56:54 +1300

    feat: collect serializer on phoenix socket connected metric

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.63.3",
+      version: "2.64.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
