3753d2b5f43fb0c031f5292d939c2640cea30bdc
Filipe Cabaço <filipecabaco@gmail.com>
2025-11-25 00:10:06 +0000

    fix up replication connection

--- test/realtime/postgres_decoder_test.exs
+++ test/realtime/postgres_decoder_test.exs
@@ -2,24 +2,22 @@ defmodule Realtime.PostgresDecoderTest do
   use ExUnit.Case, async: true
   alias Realtime.Adapters.Postgres.Decoder
 
-  alias Decoder.Messages.{
-    Begin,
-    Commit,
-    Origin,
-    Relation,
-    Relation.Column,
-    Insert,
-    Update,
-    Delete,
-    Truncate,
-    Type
-  }
+  alias Decoder.Messages.Begin
+  alias Decoder.Messages.Commit
+  alias Decoder.Messages.Origin
+  alias Decoder.Messages.Relation
+  alias Decoder.Messages.Relation.Column
+  alias Decoder.Messages.Insert
+  alias Decoder.Messages.Type
 
   test "decodes begin messages" do
     {:ok, expected_dt_no_microseconds, 0} = DateTime.from_iso8601("2019-07-18T17:02:35Z")
     expected_dt = DateTime.add(expected_dt_no_microseconds, 726_322, :microsecond)
 
-    assert Decoder.decode_message(<<66, 0, 0, 0, 2, 167, 244, 168, 128, 0, 2, 48, 246, 88, 88, 213, 242, 0, 0, 2, 107>>) ==
+    assert Decoder.decode_message(
+             <<66, 0, 0, 0, 2, 167, 244, 168, 128, 0, 2, 48, 246, 88, 88, 213, 242, 0, 0, 2, 107>>,
+             %{}
+           ) ==
              %Begin{commit_timestamp: expected_dt, final_lsn: {2, 2_817_828_992}, xid: 619}
   end
 
@@ -28,7 +26,8 @@ defmodule Realtime.PostgresDecoderTest do
     expected_dt = DateTime.add(expected_dt_no_microseconds, 726_322, :microsecond)
 
     assert Decoder.decode_message(
-             <<67, 0, 0, 0, 0, 2, 167, 244, 168, 128, 0, 0, 0, 2, 167, 244, 168, 176, 0, 2, 48, 246, 88, 88, 213, 242>>
+             <<67, 0, 0, 0, 0, 2, 167, 244, 168, 128, 0, 0, 0, 2, 167, 244, 168, 176, 0, 2, 48, 246, 88, 88, 213, 242>>,
+             %{}
            ) == %Commit{
              flags: [],
              lsn: {2, 2_817_828_992},
@@ -38,7 +37,7 @@ defmodule Realtime.PostgresDecoderTest do
   end
 
   test "decodes origin messages" do
-    assert Decoder.decode_message(<<79, 0, 0, 0, 2, 167, 244, 168, 128>> <> "Elmer Fud") ==
+    assert Decoder.decode_message(<<79, 0, 0, 0, 2, 167, 244, 168, 128>> <> "Elmer Fud", %{}) ==
              %Origin{
                origin_commit_lsn: {2, 2_817_828_992},
                name: "Elmer Fud"
@@ -48,7 +47,8 @@ defmodule Realtime.PostgresDecoderTest do
   test "decodes relation messages" do
     assert Decoder.decode_message(
              <<82, 0, 0, 96, 0, 112, 117, 98, 108, 105, 99, 0, 102, 111, 111, 0, 100, 0, 2, 0, 98, 97, 114, 0, 0, 0, 0,
-               25, 255, 255, 255, 255, 1, 105, 100, 0, 0, 0, 0, 23, 255, 255, 255, 255>>
+               25, 255, 255, 255, 255, 1, 105, 100, 0, 0, 0, 0, 23, 255, 255, 255, 255>>,
+             %{}
            ) == %Relation{
              id: 24_576,
              namespace: "public",
@@ -74,7 +74,8 @@ defmodule Realtime.PostgresDecoderTest do
   test "decodes type messages" do
     assert Decoder.decode_message(
              <<89, 0, 0, 128, 52, 112, 117, 98, 108, 105, 99, 0, 101, 120, 97, 109, 112, 108, 101, 95, 116, 121, 112,
-               101, 0>>
+               101, 0>>,
+             %{}
            ) ==
              %Type{
                id: 32_820,
@@ -83,110 +84,82 @@ defmodule Realtime.PostgresDecoderTest do
              }
   end
 
-  describe "truncate messages" do
-    test "decodes messages" do
-      assert Decoder.decode_message(<<84, 0, 0, 0, 1, 0, 0, 0, 96, 0>>) ==
-               %Truncate{
-                 number_of_relations: 1,
-                 options: [],
-                 truncated_relations: [24_576]
-               }
-    end
-
-    test "decodes messages with cascade option" do
-      assert Decoder.decode_message(<<84, 0, 0, 0, 1, 1, 0, 0, 96, 0>>) ==
-               %Truncate{
-                 number_of_relations: 1,
-                 options: [:cascade],
-                 truncated_relations: [24_576]
-               }
-    end
-
-    test "decodes messages with restart identity option" do
-      assert Decoder.decode_message(<<84, 0, 0, 0, 1, 2, 0, 0, 96, 0>>) ==
-               %Truncate{
-                 number_of_relations: 1,
-                 options: [:restart_identity],
-                 truncated_relations: [24_576]
-               }
-    end
-  end
-
   describe "data message (TupleData) decoder" do
-    test "decodes insert messages" do
+    setup do
+      relation = %Relation{
+        id: 24_576,
+        namespace: "public",
+        name: "foo",
+        replica_identity: :default,
+        columns: [
+          %Column{name: "id", type: "uuid"},
+          %Column{name: "bar", type: "text"}
+        ]
+      }
+
+      %{relation: relation}
+    end
+
+    test "decodes insert messages", %{relation: relation} do
+      uuid = UUID.uuid4()
+      string = Generators.random_string()
+
+      data =
+        "I" <>
+          <<relation.id::integer-32>> <>
+          "N" <>
+          <<2::integer-16>> <>
+          "b" <>
+          <<16::integer-32>> <>
+          UUID.string_to_binary!(uuid) <>
+          "b" <>
+          <<byte_size(string)::integer-32>> <>
+          string
+
       assert Decoder.decode_message(
-               <<73, 0, 0, 96, 0, 78, 0, 2, 98, 0, 0, 0, 3, 98, 97, 122, 98, 0, 0, 0, 3, 53, 54, 48>>
+               data,
+               %{relation.id => relation}
              ) == %Insert{
-               relation_id: 24_576,
-               tuple_data: {"baz", "560"}
+               relation_id: relation.id,
+               tuple_data: {uuid, string}
              }
     end
 
-    test "decodes insert messages with null values" do
-      assert Decoder.decode_message(<<73, 0, 0, 96, 0, 78, 0, 2, 110, 98, 0, 0, 0, 3, 53, 54, 48>>) == %Insert{
-               relation_id: 24_576,
-               tuple_data: {nil, "560"}
+    test "decodes insert messages with null values", %{relation: relation} do
+      string = Generators.random_string()
+
+      data =
+        "I" <>
+          <<relation.id::integer-32>> <>
+          "N" <>
+          <<2::integer-16>> <>
+          "n" <>
+          "b" <>
+          <<byte_size(string)::integer-32>> <>
+          string
+
+      assert Decoder.decode_message(data, %{relation.id => relation}) == %Insert{
+               relation_id: relation.id,
+               tuple_data: {nil, string}
              }
     end
 
-    test "decodes insert messages with unchanged toasted values" do
-      assert Decoder.decode_message(<<73, 0, 0, 96, 0, 78, 0, 2, 117, 98, 0, 0, 0, 3, 53, 54, 48>>) == %Insert{
-               relation_id: 24_576,
-               tuple_data: {:unchanged_toast, "560"}
-             }
-    end
+    test "decodes insert messages with unchanged toasted values", %{relation: relation} do
+      string = Generators.random_string()
 
-    test "decodes update messages with default replica identity setting" do
-      assert Decoder.decode_message(
-               <<85, 0, 0, 96, 0, 78, 0, 2, 98, 0, 0, 0, 7, 101, 120, 97, 109, 112, 108, 101, 98, 0, 0, 0, 3, 53, 54,
-                 48>>
-             ) == %Update{
-               relation_id: 24_576,
-               changed_key_tuple_data: nil,
-               old_tuple_data: nil,
-               tuple_data: {"example", "560"}
-             }
-    end
+      data =
+        "I" <>
+          <<relation.id::integer-32>> <>
+          "N" <>
+          <<2::integer-16>> <>
+          "u" <>
+          "b" <>
+          <<byte_size(string)::integer-32>> <>
+          string
 
-    test "decodes update messages with FULL replica identity setting" do
-      assert Decoder.decode_message(
-               <<85, 0, 0, 96, 0, 79, 0, 2, 98, 0, 0, 0, 3, 98, 97, 122, 98, 0, 0, 0, 3, 53, 54, 48, 78, 0, 2, 98, 0, 0,
-                 0, 7, 101, 120, 97, 109, 112, 108, 101, 98, 0, 0, 0, 3, 53, 54, 48>>
-             ) == %Update{
-               relation_id: 24_576,
-               changed_key_tuple_data: nil,
-               old_tuple_data: {"baz", "560"},
-               tuple_data: {"example", "560"}
-             }
-    end
-
-    test "decodes update messages with USING INDEX replica identity setting" do
-      assert Decoder.decode_message(
-               <<85, 0, 0, 96, 0, 75, 0, 2, 98, 0, 0, 0, 3, 98, 97, 122, 110, 78, 0, 2, 98, 0, 0, 0, 7, 101, 120, 97,
-                 109, 112, 108, 101, 98, 0, 0, 0, 3, 53, 54, 48>>
-             ) == %Update{
-               relation_id: 24_576,
-               changed_key_tuple_data: {"baz", nil},
-               old_tuple_data: nil,
-               tuple_data: {"example", "560"}
-             }
-    end
-
-    test "decodes DELETE messages with USING INDEX replica identity setting" do
-      assert Decoder.decode_message(
-               <<68, 0, 0, 96, 0, 75, 0, 2, 98, 0, 0, 0, 7, 101, 120, 97, 109, 112, 108, 101, 110>>
-             ) == %Delete{
-               relation_id: 24_576,
-               changed_key_tuple_data: {"example", nil}
-             }
-    end
-
-    test "decodes DELETE messages with FULL replica identity setting" do
-      assert Decoder.decode_message(
-               <<68, 0, 0, 96, 0, 79, 0, 2, 98, 0, 0, 0, 3, 98, 97, 122, 98, 0, 0, 0, 3, 53, 54, 48>>
-             ) == %Delete{
-               relation_id: 24_576,
-               old_tuple_data: {"baz", "560"}
+      assert Decoder.decode_message(data, %{relation.id => relation}) == %Insert{
+               relation_id: relation.id,
+               tuple_data: {:unchanged_toast, string}
              }
     end
   end
--- test/realtime/tenants/replication_connection_test.exs
+++ test/realtime/tenants/replication_connection_test.exs
@@ -243,8 +243,7 @@ defmodule Realtime.Tenants.ReplicationConnectionTest do
       topic = "db:job_scheduler"
       tenant_topic = Tenants.tenant_topic(tenant.external_id, topic, false)
       subscribe(tenant_topic, topic)
-
-      payload = job_scheduler_payload()
+      payload = %{"value" => random_string()}
 
       row =
         message_fixture(tenant, %{
@@ -605,36 +604,5 @@ defmodule Realtime.Tenants.ReplicationConnectionTest do
       |> TenantConnection.create_message(conn)
 
     message
-  end
-
-  defp job_scheduler_payload do
-    %{
-      "id" => "cdf97abd-6dca-480e-ac15-97ddf74ba714",
-      "table" => "JobScheduler",
-      "record" => %{
-        "id" => 1,
-        "status" => "ACTIVE",
-        "jobType" => "OUTBOUND_FWDISPATCH_TO_CARRIER_CHECK_CALL",
-        "timezone" => "America/Los_Angeles",
-        "createdAt" => "2025-08-17T23:04:45.264",
-        "deletedAt" => nil,
-        "updatedAt" => "2025-10-17T04:28:47.844",
-        "cronExpression" => "0 9 * * 1-5",
-        "temporaryRescheduleAt" => nil
-      },
-      "schema" => "public",
-      "operation" => "UPDATE",
-      "old_record" => %{
-        "id" => 1,
-        "status" => "ACTIVE",
-        "jobType" => "OUTBOUND_FWDISPATCH_TO_CARRIER_CHECK_CALL",
-        "timezone" => "America/Los_Angeles",
-        "createdAt" => "2025-08-17T23:04:45.264",
-        "deletedAt" => nil,
-        "updatedAt" => "2025-10-17T04:28:47.843",
-        "cronExpression" => "0 9 * * 1-5",
-        "temporaryRescheduleAt" => nil
-      }
-    }
   end
 end
