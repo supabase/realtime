3f840c1d094115552c992eb2d23c1556d8b42721
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2025-12-10 14:04:12 +1300

    fix: improve metrics controller
    * Allow 3x more max heap size than other processes
    * Avoid compressing given that gen_rpc does this for us already

--- lib/realtime_web/controllers/metrics_controller.ex
+++ lib/realtime_web/controllers/metrics_controller.ex
@@ -4,40 +4,50 @@ defmodule RealtimeWeb.MetricsController do
   alias Realtime.PromEx
   alias Realtime.GenRpc
 
+  # We give more memory and time to collect metrics from all nodes as this is a lot of work
   def index(conn, _) do
-    timeout = Application.fetch_env!(:realtime, :metrics_rpc_timeout)
-
-    cluster_metrics =
-      Node.list()
-      |> Task.async_stream(
-        fn node ->
-          {node, GenRpc.call(node, PromEx, :get_compressed_metrics, [], timeout: timeout)}
-        end,
-        timeout: :infinity
-      )
-      |> Enum.reduce([PromEx.get_metrics()], fn {_, {node, response}}, acc ->
-        case response do
-          {:error, :rpc_error, reason} ->
-            Logger.error("Cannot fetch metrics from the node #{inspect(node)} because #{inspect(reason)}")
-            acc
-
-          metrics ->
-            [uncompress(metrics) | acc]
-        end
-      end)
-      |> Enum.reverse()
+    {time, metrics} = :timer.tc(&cluster_metrics/0, :millisecond)
+    Logger.info("Collected cluster metrics in #{time} milliseconds")
 
     conn
     |> put_resp_content_type("text/plain")
-    |> send_resp(200, cluster_metrics)
+    |> send_resp(200, metrics)
   end
 
-  defp uncompress(compressed_data) do
-    :zlib.uncompress(compressed_data)
-  rescue
-    error ->
-      Logger.error("Failed to decompress metrics data: #{inspect(error)}")
-      # Return empty string to not impact the aggregated metrics
-      ""
+  defp cluster_metrics() do
+    bump_max_heap_size()
+    timeout = Application.fetch_env!(:realtime, :metrics_rpc_timeout)
+
+    Node.list()
+    |> Task.async_stream(
+      fn node ->
+        {node, GenRpc.call(node, __MODULE__, :get_metrics, [], timeout: timeout)}
+      end,
+      timeout: :infinity
+    )
+    |> Enum.reduce([PromEx.get_metrics()], fn {_, {node, response}}, acc ->
+      case response do
+        {:error, :rpc_error, reason} ->
+          Logger.error("Cannot fetch metrics from the node #{inspect(node)} because #{inspect(reason)}")
+          acc
+
+        metrics ->
+          [metrics | acc]
+      end
+    end)
+  end
+
+  def get_metrics() do
+    bump_max_heap_size()
+    PromEx.get_metrics()
+  end
+
+  defp bump_max_heap_size() do
+    system_max_heap_size = :erlang.system_info(:max_heap_size)[:size]
+
+    # it's 0 when there is no limit
+    if is_integer(system_max_heap_size) and system_max_heap_size > 0 do
+      Process.flag(:max_heap_size, system_max_heap_size * 3)
+    end
   end
 end
