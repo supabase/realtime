e86e9849ad92874b55ac7ed1e4ab55be702a49cb
Eduardo Gurgel <eduardo.gurgel@supabase.io>
2026-01-30 13:31:08 +1300

    fix: use beacon on connected tenant metrics (#1673)
    Also fix the mix.exs version 🙈

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Realtime.MixProject do
   def project do
     [
       app: :realtime,
-      version: "2.74.0",
+      version: "2.73.5",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
