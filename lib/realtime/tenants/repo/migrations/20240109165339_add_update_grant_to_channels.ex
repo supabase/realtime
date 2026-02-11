1c203a5531f93437e317697845c450b823735d4c
Filipe Cabaço <filipecabaco@gmail.com>
2026-02-11 17:53:06 +0000

    feat: squash migrations
    squashes all migrations to reduce migration time

--- lib/realtime/tenants/repo/migrations/20240109165339_add_update_grant_to_channels.ex
+++ lib/realtime/tenants/repo/migrations/20240109165339_add_update_grant_to_channels.ex
@@ -1,11 +1,5 @@
 defmodule Realtime.Tenants.Migrations.AddUpdateGrantToChannels do
   @moduledoc false
-
   use Ecto.Migration
-
-  def change do
-    execute("""
-    GRANT UPDATE ON realtime.channels TO postgres, anon, authenticated, service_role
-    """)
-  end
+  def change, do: nil
 end
