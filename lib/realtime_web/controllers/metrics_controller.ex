69178464db269a0fc5f14da0efbae30cc652a3bb
Filipe Cabaço <filipe@supabase.io>
2026-04-06 19:50:22 +0100

    fix: remove double endpoint for metrics (#1794)
    with push metrics available this code is no longer required and adds bloat to the system

--- lib/realtime_web/controllers/metrics_controller.ex
+++ lib/realtime_web/controllers/metrics_controller.ex
@@ -6,70 +6,62 @@ defmodule RealtimeWeb.MetricsController do
   alias Realtime.GenRpc
 
   def index(conn, _) do
-    serve_metrics(conn, [Node.self() | Node.list()], :get_global_metrics, "global cluster")
-  end
-
-  def tenant(conn, _) do
-    serve_metrics(conn, [Node.self() | Node.list()], :get_tenant_metrics, "tenant cluster")
+    serve_metrics(conn, [Node.self() | Node.list()], "cluster")
   end
 
   def region(conn, %{"region" => region}) do
-    serve_metrics(conn, Realtime.Nodes.region_nodes(region), :get_global_metrics, "global region=#{region}")
+    serve_metrics(conn, Realtime.Nodes.region_nodes(region), "region=#{region}")
   end
 
-  def region_tenant(conn, %{"region" => region}) do
-    serve_metrics(conn, Realtime.Nodes.region_nodes(region), :get_tenant_metrics, "tenant region=#{region}")
-  end
-
-  defp serve_metrics(conn, nodes, metrics_fun, label) do
+  defp serve_metrics(conn, nodes, label) do
     conn =
       conn
       |> put_resp_content_type("text/plain")
       |> send_chunked(200)
 
-    {time, conn} = :timer.tc(fn -> collect_metrics(nodes, metrics_fun, conn) end, :millisecond)
+    {time, conn} = :timer.tc(fn -> collect_metrics(nodes, conn) end, :millisecond)
     Logger.info("Collected #{label} metrics in #{time} milliseconds")
 
     conn
   end
 
-  defp collect_metrics(nodes, metrics_fun, conn) do
+  defp collect_metrics(nodes, conn) do
     bump_max_heap_size()
     timeout = Application.fetch_env!(:realtime, :metrics_rpc_timeout)
 
     nodes
     |> Task.async_stream(
       fn node ->
-        {node, GenRpc.call(node, __MODULE__, metrics_fun, [], timeout: timeout)}
+        {node, GenRpc.call(node, __MODULE__, :get_metrics, [], timeout: timeout)}
       end,
       timeout: :infinity
     )
-    |> Enum.reduce(conn, fn {_, {node, response}}, acc_conn ->
-      case response do
-        {:error, :rpc_error, reason} ->
-          Logger.error("Cannot fetch metrics from the node #{inspect(node)} because #{inspect(reason)}")
-          acc_conn
+    |> Enum.reduce(conn, fn
+      {:ok, {node, {:error, :rpc_error, reason}}}, acc_conn ->
+        Logger.error("Cannot fetch metrics from the node #{inspect(node)} because #{inspect(reason)}")
+        acc_conn
 
-        metrics ->
-          {:ok, acc_conn} = chunk(acc_conn, metrics)
-          :erlang.garbage_collect()
-          acc_conn
-      end
+      {:ok, {_node, metrics}}, acc_conn ->
+        case chunk(acc_conn, metrics) do
+          {:ok, acc_conn} ->
+            :erlang.garbage_collect()
+            acc_conn
+
+          {:error, reason} ->
+            Logger.error("Cannot stream metrics chunk because #{inspect(reason)}")
+            acc_conn
+        end
+
+      {:exit, reason}, acc_conn ->
+        Logger.error("Metrics collection task exited: #{inspect(reason)}")
+        acc_conn
     end)
   end
 
-  def get_global_metrics do
+  def get_metrics do
     bump_max_heap_size()
-    PromEx.get_global_metrics()
+    [PromEx.get_global_metrics(), TenantPromEx.get_metrics()]
   end
-
-  def get_tenant_metrics do
-    bump_max_heap_size()
-    TenantPromEx.get_metrics()
-  end
-
-  @doc deprecated: "Use get_global_metrics/0 instead"
-  def get_metrics, do: get_global_metrics()
 
   defp bump_max_heap_size do
     system_max_heap_size = :erlang.system_info(:max_heap_size)[:size]
