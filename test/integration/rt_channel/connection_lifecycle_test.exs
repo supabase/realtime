e9aeed738b49c91232115e377d1a0d809292c439
Filipe Cabaço <filipecabaco@gmail.com>
2026-04-21 01:41:12 +0100

    fix: properly match nil errors on user socket

--- test/integration/rt_channel/connection_lifecycle_test.exs
+++ test/integration/rt_channel/connection_lifecycle_test.exs
@@ -18,6 +18,33 @@ defmodule Realtime.Integration.RtChannel.ConnectionLifecycleTest do
 
   setup [:checkout_tenant_and_connect]
 
+  describe "socket connect - tenant not found" do
+    test "logs TenantNotFound and rejects connection for unknown external_id", %{serializer: serializer} do
+      fake_tenant = %{external_id: "nonexistent-#{System.unique_integer([:positive])}"}
+
+      log =
+        capture_log(fn ->
+          assert {:error, _} =
+                   WebsocketClient.connect(self(), uri(fake_tenant, serializer), serializer, [
+                     {"x-api-key", "some-token"}
+                   ])
+        end)
+
+      assert log =~ "TenantNotFound"
+    end
+  end
+
+  describe "socket connect - missing api key" do
+    test "logs MissingAPIKey and rejects connection when no token provided", %{tenant: tenant, serializer: serializer} do
+      log =
+        capture_log(fn ->
+          assert {:error, _} = WebsocketClient.connect(self(), uri(tenant, serializer), serializer, [])
+        end)
+
+      assert log =~ "MissingAPIKey"
+    end
+  end
+
   describe "socket disconnect - tenant suspension" do
     setup [:rls_context]
 
