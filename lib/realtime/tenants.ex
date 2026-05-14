37daee0476636bea7248c5f3039af48a8083b77d
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2026-05-14 14:20:14 +1200

    fix: avoid using BatchBroadcast from ReplicationConnection
    This way we have a more straightforward approach to then add support to
    binary payloads without changing BatchBroadcast

--- lib/realtime/tenants.ex
+++ lib/realtime/tenants.ex
@@ -491,7 +491,7 @@ defmodule Realtime.Tenants do
 
   @doc """
   """
-  @spec validate_payload_size(Tenant.t() | binary(), map()) :: :ok | {:error, :payload_size_exceeded}
+  @spec validate_payload_size(Tenant.t() | binary(), map() | binary()) :: :ok | {:error, :payload_size_exceeded}
   def validate_payload_size(tenant_id, payload) when is_binary(tenant_id) do
     tenant_id
     |> Cache.get_tenant_by_external_id()
