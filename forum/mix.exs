d217d452ce7f043317c0fcafaf0ce1da71d6aa9e
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2026-06-11 11:48:35 +1200

    wip

--- forum/mix.exs
+++ forum/mix.exs
@@ -28,6 +28,9 @@ defmodule Forum.MixProject do
     [
       {:telemetry, "~> 1.3"},
       {:ex_hash_ring, "~> 6.0"},
+      # Trace-based testing. The Elixir `Snabbkaffe` interface (lib/snabbkaffe.ex)
+      # wraps these Erlang macros so trace points are discarded in non-test builds.
+      {:snabbkaffe, "~> 1.0"},
       {:mimic, "~> 1.0", only: :test},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
