249246d990188e0cad2afbac4a3207590d903601
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2026-07-23 10:37:57 +1200

    chore: update dialyzer_ignore

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,10 @@
 # https://github.com/elixir-ecto/postgrex/pull/784
 [
   {"lib/extensions/postgres_cdc_rls/replications.ex", :call},
-  {"lib/extensions/postgres_cdc_rls/replications.ex", :no_return}
+  {"lib/extensions/postgres_cdc_rls/replications.ex", :no_return},
+  {"lib/extensions/postgres_cdc_rls/subscriptions.ex", :call},
+  {"lib/extensions/postgres_cdc_rls/subscriptions.ex", :no_return},
+  # params_to_log/1 is only reached from the error branches that the no_return
+  # cascade above makes Dialyzer treat as dead, so it is wrongly flagged unused.
+  {"lib/extensions/postgres_cdc_rls/subscriptions.ex", :unused_fun}
 ]
