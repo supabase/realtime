b9bc72f2777bf3b714bd37c8ead731fdbbef0120
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2026-07-22 16:32:25 +1200

    chore: add temporary dialyzer ignore

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,5 @@
+# https://github.com/elixir-ecto/postgrex/pull/784
+[
+  {"lib/extensions/postgres_cdc_rls/replications.ex", :call},
+  {"lib/extensions/postgres_cdc_rls/replications.ex", :no_return}
+]
