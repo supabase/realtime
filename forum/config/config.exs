03bf163ff74964588db94e71bc876f0dd884393d
Eduardo Gurgel Pinho <eduardo.gurgel@supabase.io>
2026-08-06 12:25:29 +1200

    fix: logger config backends deprecation

--- forum/config/config.exs
+++ forum/config/config.exs
@@ -1,4 +1,4 @@
 import Config
 
 # Print nothing during tests unless captured or a test failure happens
-config :logger, backends: [], level: :debug
+config :logger, level: :debug, default_handler: false
