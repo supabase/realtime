7a91272d32bba2d759810d0d4bde159d0ea4e1d8
Leandro Pereira <leandro@leandro.io>
2026-08-19 12:27:13 -0400

    fix: add missing :listeners config (#2115)

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule Realtime.MixProject do
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix_live_view] ++ Mix.compilers(),
+      listeners: [Phoenix.CodeReloader],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
