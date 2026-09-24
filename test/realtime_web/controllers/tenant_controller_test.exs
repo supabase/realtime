f3b02dbc194d49e9e334c777e3eba4fee3a960d3
Mandar Joshi <mandarjoshi1045@gmail.com>
2026-09-25 00:17:47 +0530

    test: use slot_name filter in tenant_controller_test

--- test/realtime_web/controllers/tenant_controller_test.exs
+++ test/realtime_web/controllers/tenant_controller_test.exs
@@ -317,10 +317,12 @@ defmodule RealtimeWeb.TenantControllerTest do
       assert Cache.get_tenant_by_external_id(tenant.external_id)
       {:ok, db_conn} = Database.connect(tenant, "realtime_test", :stop)
 
-      %{rows: rows} =
-        Postgrex.query!(db_conn, "SELECT slot_name FROM pg_replication_slots WHERE database = current_database()", [])
+      slot_name = Realtime.Tenants.ReplicationConnection.replication_slot_name("realtime", "messages")
 
-      assert length(rows) > 0
+      %{rows: rows} =
+        Postgrex.query!(db_conn, "SELECT slot_name FROM pg_replication_slots WHERE slot_name = $1", [slot_name])
+
+      assert length(rows) == 1
       conn = delete(conn, ~p"/api/tenants/#{tenant.external_id}")
       assert response(conn, 204)
 
@@ -331,8 +333,8 @@ defmodule RealtimeWeb.TenantControllerTest do
       assert_eventually {:ok, %{rows: []}} =
                           Postgrex.query(
                             db_conn,
-                            "SELECT slot_name FROM pg_replication_slots WHERE database = current_database()",
-                            []
+                            "SELECT slot_name FROM pg_replication_slots WHERE slot_name = $1",
+                            [slot_name]
                           )
     end
 
